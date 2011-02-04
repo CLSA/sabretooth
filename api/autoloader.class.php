07d511ce36fce39fa47dfcc8db67fdd931b0a17e
Patrick Emond <emondpd@mcmaster.ca>
2011-02-04 12:03:32 -0500

    Adding in missing documentation.

--- api/autoloader.class.php
+++ api/autoloader.class.php
@@ -1,13 +1,39 @@
 <?php
+/**
+ * autoloader.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @package sabretooth
+ */
+
 namespace sabretooth;
+
+/**
+ * Autoloader class which automatically includes project class files.
+ * @package sabretooth
+ */
 class autoloader
 {
+  /**
+   * Registers this class with PHP as an autoloader.
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @static
+   * @access public
+   */
   static public function register()
   {
     ini_set( 'unserialize_callback_func', 'spl_autoload_call' );
     spl_autoload_register( array( new self, 'autoload' ) );
   }
 
+  /**
+   * This method is called by PHP whenever an undefined class is used.
+   * If the class is in the sabretooth\ namespace it attemps to load it from the api/ directory.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @static
+   * @access public
+   */
   static public function autoload( $class )
   {
     // only work on classes starting with sabretooth\
