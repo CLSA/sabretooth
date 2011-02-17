eadf947a3c697de68b3ee5a9adbd70e72fb863cd
Patrick Emond <emondpd@mcmaster.ca>
2011-02-17 14:33:28 -0500

    Further implementation of list add/remove.

---
+++ api/ui/base_delete_record.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * base_delete_record.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @package sabretooth\ui
+ * @filesource
+ */
+
+namespace sabretooth\ui;
+
+/**
+ * Base class for all "delete_record" actions.
+ * 
+ * @package sabretooth\ui
+ */
+abstract class base_delete_record extends base_record_action
+{
+  /**
+   * Constructor.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $subject The widget's subject.
+   * @param string $child The list item's subject.
+   * @param array $args Action arguments
+   * @access public
+   */
+  public function __construct( $subject, $child, $args )
+  {
+    parent::__construct( $subject, 'delete_'.$child, $args );
+    $this->child_subject = $child;
+  }
+  
+  /**
+   * Executes the action.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function execute()
+  {
+    $method_name = 'remove_'.$this->child_subject;
+    $this->get_record()->$method_name( $this->get_argument( 'id_list' ) );
+  }
+
+  /**
+   * The list item's subject.
+   * @var string
+   * @access protected
+   */
+  protected $child_subject = '';
+}
+?>
---
+++ api/ui/role_delete_operation.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * role_delete_operation.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @package sabretooth\ui
+ * @filesource
+ */
+
+namespace sabretooth\ui;
+
+/**
+ * action role delete_operation
+ * 
+ * @package sabretooth\ui
+ */
+class role_delete_operation extends base_delete_record
+{
+  /**
+   * Constructor.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param array $args Action arguments
+   * @access public
+   */
+  public function __construct( $args )
+  {
+    parent::__construct( 'role', 'operation', $args );
+  }
+}
+?>
