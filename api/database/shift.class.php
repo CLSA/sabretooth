9734aa32df48a40e6f39ae99ccbcdba0222de648
Patrick Emond <emondpd@mcmaster.ca>
2011-03-14 19:04:37 -0400

    Working on start/end time logic [WIP]

--- api/database/shift.class.php
+++ api/database/shift.class.php
@@ -14,5 +14,67 @@ namespace sabretooth\database;
  *
  * @package sabretooth\database
  */
-class shift extends record {}
+class shift extends record
+{
+  /**
+   * Overrides the parent class to prevent doubling shift times.
+   * 
+   * If the record has a stage which already exists it will push the current phase and all that
+   * come after it down by one stage to make room for this one.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @throws exception\runtime
+   * @access public
+   */
+  public function save()
+  {
+    // warn if we are in read-only mode
+    if( $this->read_only )
+    {
+      \sabretooth\log::warning( 'Tried to save read-only record.' );
+      return;
+    }
+    
+    // make sure start time comes after end time
+    $start_obj = new \DateTime( $this->start_time );
+    $end_obj = new \DateTime( $this->end_time );
+    $interval = $start_obj->diff( $end_obj );
+    if( 0 != $interval->invert ||
+        ( 0 == $interval->days && 0 == $interval->h && 0 == $interval->i && 0 == $interval->s ) )
+    {
+      throw new \sabretooth\exception\runtime(
+        'Tried to set end time which is not after the start time in shift record.', __METHOD__ );
+    }
+
+    // convert the start and end times to server time
+    $start_time = \sabretooth\util::to_server_time( $this->start_time );
+    $end_time = \sabretooth\util::to_server_time( $this->end_time );
+    
+    // See if the user already has a shift at this time
+    $modifier = new modifier();
+    $modifier->where( 'id', '!=', $this->id );
+    $modifier->where( 'user_id', '=', $this->user_id );
+    $modifier->where( 'date', '=', $this->date );
+    
+    // (need to use custom SQL)
+    $count = static::db()->get_one( 
+      sprintf( 'SELECT COUNT(*) FROM %s %s '.
+               'AND NOT ( ( start_time <= %s AND end_time <= %s ) OR '.
+                         '( start_time >= %s AND end_time >= %s ) )',
+               static::get_table_name(),
+               $modifier->get_where(),
+               database::format_string( $start_time ),
+               database::format_string( $start_time ),
+               database::format_string( $end_time ),
+               database::format_string( $end_time ) ) );
+    
+    if( 0 < $count )
+    {
+      throw new \sabretooth\exception\runtime(
+        'Tried to create shift which overlaps with another shift\'s time.', __METHOD__ );
+    }
+    
+    // all is well, continue with the parent's save method
+    parent::save();
+  }
+}
 ?>
--- api/ui/shift_edit.class.php
+++ api/ui/shift_edit.class.php
@@ -19,13 +19,60 @@ class shift_edit extends base_edit
 {
   /**
    * Constructor.
-   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @autho Patrick Emond <emondpd@mcmaster.ca>
    * @param array $args Action arguments
    * @access public
    */
   public function __construct( $args )
   {
     parent::__construct( 'shift', $args );
+  }
+
+  /**
+   * Executes the action.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function execute()
+  {
+    $column = $this->get_argument( 'columns', array() );
+
+    // TODO: manage start/end times
+    //       If start time moves, move end time.
+    //       If end time moves, make sure it's after start time
+    //       Find other instances of start/end times and apply there as well
+    /*
+    if( array_key_exists( 'start_time', $columns ) )
+    {
+      $start_value = $columns['start_time'];
+      $end_value = array_key_exists( 'end_time', $columns )
+                 ? $columns['end_time']
+                 : $this->get_record()->end_time;
+
+      if( strtotime( $start_value ) >= strtotime( $end_value ) )
+      {
+        throw new \sabretooth\exception\notice(
+          sprintf( 'Make sure that the start time comes before the end time' ),
+          __METHOD__ );
+      }
+    }
+    */
+    try
+    {
+      parent::execute();
+    }
+    catch( \sabretooth\exception\runtime $e )
+    { // TODO: help describe exceptions to the user
+      //       This is will happen in other places (appointments, for instance) so it should
+      //       Maybe not live here?
+      /*
+      throw new \sabretooth\exception\notice(
+        sprintf( 'Unable to set time from %s to %s, another shift is during that period.',
+                 $TODO,
+                 $TODO ), __METHOD__, $e ); */
+
+      throw $e;
+    }
   }
 }
 ?>
