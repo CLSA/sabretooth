793bc5fd68f2d212e1a5fcfa3b89b1ff9f1f8665
Patrick Emond <emondpd@mcmaster.ca>
2011-03-22 13:23:36 -0400

    Allowing queue restriction for admins.

--- api/ui/queue_list.class.php
+++ api/ui/queue_list.class.php
@@ -46,12 +46,29 @@ class queue_list extends base_list_widget
     parent::finish();
     
     $session = \sabretooth\session::self();
+    $is_administrator = 'administrator' == $session->get_role()->name;
     $is_supervisor = 'supervisor' == $session->get_role()->name;
+    
+    // if this is an admin, give them a list of sites to choose from
+    if( $is_administrator )
+    {
+      $sites = array();
+      foreach( \sabretooth\database\site::select() as $db_site )
+        $sites[$db_site->id] = $db_site->name;
+      $this->set_variable( 'sites', $sites );
+    }
+    
+    $restrict_site_id = $this->get_argument( "restrict_site_id", 0 );
+    $this->set_variable( 'restrict_site_id', $restrict_site_id );
+    $db_restrict_site = $restrict_site_id
+                      ? new \sabretooth\database\site( $restrict_site_id )
+                      : NULL;
 
     foreach( $this->get_record_list() as $record )
     {
       // restrict to the current site if the current user is a supervisor
       if( $is_supervisor ) $record->set_site( $session->get_site() );
+      else if( !is_null( $db_restrict_site ) ) $record->set_site( $db_restrict_site );
 
       $db_setting = \sabretooth\database\setting::get_setting( 'queue state', $record->name );
       $this->add_row( $record->id,
