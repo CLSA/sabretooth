c989c9c88c5838cf274dc780ae153286bc0bbe3d
Patrick Emond <emondpd@mcmaster.ca>
2011-04-19 14:06:26 -0400

    Validating contact phone numbers.

--- api/ui/contact_edit.class.php
+++ api/ui/contact_edit.class.php
@@ -27,5 +27,25 @@ class contact_edit extends base_edit
   {
     parent::__construct( 'contact', $args );
   }
+
+  /**
+   * Executes the action.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function execute()
+  {
+    $columns = $this->get_argument( 'columns' );
+
+    // if there is a phone number, validate it
+    if( $columns['phone'] )
+    {
+      if( 10 != strlen( preg_replace( '/[^0-9]/', '', $columns['phone'] ) ) )
+        throw new \sabretooth\exception\notice(
+          'Phone number must have exactly 10 digits.', __METHOD__ );
+    }
+
+    parent::execute();
+  }
 }
 ?>
--- api/ui/contact_new.class.php
+++ api/ui/contact_new.class.php
@@ -27,5 +27,29 @@ class contact_new extends base_new
   {
     parent::__construct( 'contact', $args );
   }
+  
+  /**
+   * Executes the action.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function execute()
+  {
+    // make sure there is either a phone number OR city and province
+    $columns = $this->get_argument( 'columns' );
+    if( !( $columns['phone'] || ( $columns['province_id'] && $columns['city'] ) ) )
+      throw new \sabretooth\exception\notice(
+        'You must provide a phone number OR city and province.', __METHOD__ );
+    
+    // if there is a phone number, validate it
+    if( $columns['phone'] )
+    {
+      if( 10 != strlen( preg_replace( '/[^0-9]/', '', $columns['phone'] ) ) )
+        throw new \sabretooth\exception\notice(
+          'Phone number must have exactly 10 digits.', __METHOD__ );
+    }
+
+    parent::execute();
+  }
 }
 ?>
