3b14353b6069619611931a046f1e1619abfd4f5e
Patrick Emond <emondpd@mcmaster.ca>
2011-04-21 18:13:27 -0400

    Started working on site feed.

--- api/ui/site_feed.class.php
+++ api/ui/site_feed.class.php
@@ -42,8 +42,82 @@ class site_feed extends base_feed
    */
   public function get_data()
   {
-    // TODO: implement
+    $db_site = bus\session::self()->get_site();
+
+    // start by creating an array with one element per day in the time span
+    $start_datetime = new \DateTime( $this->start_date );
+    $end_datetime = new \DateTime( $this->end_date );
+    
+    $days = array();
+    $current_datetime = $start_datetime;
+    while( $current_datetime->diff( $end_datetime )->days )
+    {
+      $days[ $current_datetime->format( 'Y-m-d' ) ][ '00:00' ] = 0;
+      $current_datetime->add( new \DateInterval( 'P1D' ) );
+    }
+    
+    // now incorperate operator shifts
+    $modifier = new db\modifier();
+    $modifier->where( 'site_id', '=', $db_site->id );
+    $modifier->where( 'date', '>=', $this->start_date );
+    $modifier->where( 'date', '<', $this->end_date );
+    $modifier->order( 'date' );
+
+    foreach( db\shift::select( $modifier ) as $db_shift )
+    {
+      // cut off seconds from start and end time
+      $start = substr( $db_shift->start_time, 0, -3 );
+      $start_obj = new \DateTime( $start );
+      $end = substr( $db_shift->end_time, 0, -3 );
+
+      // loop through this day's times until we pass the start time
+      foreach( $days[ $db_shift->date ] as $time => $slots )
+      {
+        $diff = $start_obj->diff( new \DateTime( $time ) );
+
+        if( 0 == $diff->h && 0 == $diff->i )
+        { // equal to the current time
+          
+        }
+        else if( $start_obj->diff( new \DateTime( $time ) )->invert )
+        { // passed the current time
+        }
+      }
+    }
+
     return array();
+    /*
+    $modifier = new db\modifier();
+    $modifier->where( 'date', '>=', $this->start_date );
+    $modifier->where( 'date', '<', $this->end_date );
+    $modifier->order( 'date' );
+    
+    $event_list = array();
+
+
+    $current_datetime = new \DateTime( $this->start_date );
+    foreach( db\appointment::select_for_site( $db_site, $modifier ) as $db_appointment )
+    {
+      // fill time up to the appointment's date
+      $appointment_datetime = new \DateTime( $db_appointment->date );
+      $interval = $current_datetime->diff( $appointment_datetime );
+      if( $interval->days )
+      {
+      }
+
+      // set the current time to the start of this appointment
+      $current_datetime = $appointment_datetime;
+    }
+
+    // fill time up to the end of the time period
+    $event_list[] = array(
+      'title' => 'Slots: '.$available,
+      'allDay' => true,
+      'start' => $current_datetime->getTimestamp(),
+      'end' => strtotime( $this->end_date );
+
+    return $event_list;
+    */
   }
 }
 ?>
