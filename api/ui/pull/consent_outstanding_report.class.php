7562aef881a3f30bb78b07f74b0b921913e9bb25
Dean Inglis <inglisd@mcmaster.ca>
2011-07-25 13:52:22 -0400

    written consent outstanding WIP

--- api/ui/pull/consent_outstanding_report.class.php
+++ api/ui/pull/consent_outstanding_report.class.php
@@ -47,59 +47,37 @@ class consent_outstanding_report extends base_report
 
     $contents = array();
 
+
     // loop through participants searching for those who have completed their most recent interview
     foreach( db\participant::select() as $db_participant )
     {
       // dont bother with deceased or otherwise impaired
       if( !is_null( $db_participant->status ) ) continue;
 
-      
-      // only grab the first interview for now
-      $interview_list = $db_participant->get_interview_list();
-      
-      if( 0 == count( $interview_list ) ) continue;
-
-      $done = false;
-
-      $db_interview = current( $interview_list );
-      if( $db_interview->completed )
+      $consent_mod = new db\modifier();
+      $consent_mod->where( 'event', '=', 'verbal accept' );
+      if( 0 == count( $db_participant->get_consent_list( $consent_mod ) ) )
       {
-        $db_consent = $db_participant->get_last_consent();
+        // only grab the first interview for now
+        $interview_list = $db_participant->get_interview_list();
+      
+        if( 0 == count( $interview_list ) ) continue;
 
-       /// get_consent_list
-       // loop through them checking for 
-
-        // if they finished the interview, can they have 
-        // a denied or null consent?
-        // should this be just non null consent with
-        // verbal accept only? otherwise why request written consent if they denied it?
-        if( !is_null( $db_consent ) && !(
-            'written deny'   == $db_consent->event ||
-            'written accept' == $db_consent->event ||
-            'retract' == $db_consent->event ||
-            'withdraw' == $db_consent->event ) )
+        $db_interview = current( $interview_list );
+        if( $db_interview->completed )
         {
-          $done = true;
-        }          
-      }
-      
-      if( $done )
-      {
-        // should this be primary or first address?
-        $db_address = $db_participant->get_primary_address();
-        $db_region = $db_address->get_region();
-        // is this how we get DATE OF COMPLETION?
-        // the returned phone call is either null or a new phone_call object
-        $db_last_phone_call = $db_participant->get_last_contacted_phone_call();
+          $db_address = $db_participant->get_primary_address();
+          $db_region = $db_address->get_region();
+          $db_last_phone_call = $db_participant->get_last_contacted_phone_call();
         
-        $date_completed = 'NA';
-        if( $db_last_phone_call )
-        {
-          $date_completed = substr( $db_last_phone_call->start_datetime, 0, 
-            strpos( $db_last_phone_call->start_datetime, ' ' ) );
-        }
+          $date_completed = 'NA';
+         if( $db_last_phone_call )
+         {
+            $date_completed = substr( $db_last_phone_call->start_datetime, 0, 
+              strpos( $db_last_phone_call->start_datetime, ' ' ) );
+         }
 
-        $contents[] = array(
+          $contents[] = array(
             $db_participant->uid,
             $db_participant->first_name,
             $db_participant->last_name,
@@ -109,7 +87,8 @@ class consent_outstanding_report extends base_report
             $db_region->country,
             $db_address->postcode,
             $date_completed );
-      }
+        }
+      } // end non-empty consent list search  
     } // end loop on participants 
 
     
