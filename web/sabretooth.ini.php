1fc3a867e95f70952a13e34186ff4f6b1eb575d0
Patrick Emond <emondpd@mcmaster.ca>
2011-07-28 11:47:08 -0400

    Bumped version number to 0.2.2

--- web/sabretooth.ini.php
+++ web/sabretooth.ini.php
@@ -14,7 +14,7 @@ namespace sabretooth;
 global $SETTINGS;
 
 // tagged version
-$SETTINGS[ 'general' ][ 'version' ] = '0.2.1';
+$SETTINGS[ 'general' ][ 'version' ] = '0.2.2';
 
 // always leave as false when running as production server
 $SETTINGS[ 'general' ][ 'development_mode' ] = false;
