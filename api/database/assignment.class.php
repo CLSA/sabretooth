053ffa58a00e2a5b465a56b9a891e717e2b2d8fe
Patrick Emond <emondpd@mcmaster.ca>
2011-08-04 17:30:39 -0400

    Adding Mastodon information to tokens.
    Limesurvey's token management is now being used to fill dynamic
    survey text instead of javascript.  Furthermore, the information
    is being fetched from Mastodon.

--- api/database/assignment.class.php
+++ api/database/assignment.class.php
@@ -37,21 +37,27 @@ class assignment extends has_note
       return NULL;
     } 
     
+    $db_interview = $this->get_interview();
+
     // if the interview is complete then there is no valid token
-    if( $this->get_interview()->completed ) return NULL;
+    if( $db_interview->completed ) return NULL;
 
     if( is_null( $this->current_phase ) )
     { // we haven't determined the current phase yet, do that now using tokens
       $modifier = new modifier();
       $modifier->order( 'rank' );
       
-      $phase_list = $this->get_interview()->get_qnaire()->get_phase_list( $modifier );
+      $phase_list = $db_interview->get_qnaire()->get_phase_list( $modifier );
       if( 0 == count( $phase_list ) )
       {
         log::emerg( 'Questionnaire with no phases has been assigned.' );
         return NULL;
       }
       
+      $db_participant = $db_interview->get_participant();
+      $db_site = bus\session::self()->get_site();
+      $db_role = bus\session::self()->get_role();
+      $db_user = bus\session::self()->get_user();
 
       foreach( $phase_list as $db_phase )
       {
@@ -67,10 +73,144 @@ class assignment extends has_note
 
         if( is_null( $completed ) )
         { // token not found, create it
+
+          // set the token string, first name, last name and email for all tokens
+          $sets = sprintf( 'token = %s', database::format_string( $token ) );
+          $sets .= sprintf( ', firstname = %s', database::format_string(
+            $db_participant->first_name ) );
+          $sets .= sprintf( ', lastname = %s', database::format_string(
+            $db_participant->last_name ) );
+
+          // repeated phases require extra token information
+          if( $db_phase->repeated )
+          {
+            // determine mastodon's base url (using basic authentication)
+            $base_url = SABRETOOTH_URL.'/'.MASTODON_URL.'/';
+            $base_url = preg_replace(
+              '#://#', '://'.$_SERVER['PHP_AUTH_USER'].':'.$_SERVER['PHP_AUTH_PW'].'@', $base_url );
+
+            $request = new \HttpRequest();
+            $request->enableCookies();
+            
+            // set the site
+            $request->setUrl( $base_url.'self/set_site' );
+            $request->setMethod( \HttpRequest::METH_POST );
+            $request->setPostFields( array( 'name' => $db_site->name, 'cohort' => 'tracking' ) );
+
+            if( 200 != $request->send()->getResponseCode() )
+              throw new exc\runtime( 'Unable to connect to Mastodon', __METHOD__ );
+            
+            // set the role
+            $request->setUrl( $base_url.'self/set_role' );
+            $request->setMethod( \HttpRequest::METH_POST );
+            $request->setPostFields( array( 'name' => $db_role->name ) );
+            if( 200 != $request->send()->getResponseCode() )
+              throw new exc\runtime( 'Unable to connect to Mastodon', __METHOD__ );
+            
+            // get the participant's primary information
+            $request->setUrl( $base_url.'participant/primary' );
+            $request->setMethod( \HttpRequest::METH_GET );
+            $request->setQueryData( array( 'uid' => $db_participant->uid ) );
+            $message = $request->send();
+            if( 200 != $message->getResponseCode() )
+              throw new exc\runtime( 'Unable to fetch participant info from Mastodon', __METHOD__ );
+            $participant_info = json_decode( $message->getBody() );
+            
+            // get the participant's consent information
+            $request->setUrl( $base_url.'participant/list_consent' );
+            $request->setMethod( \HttpRequest::METH_GET );
+            $request->setQueryData( array( 'uid' => $db_participant->uid ) );
+            $message = $request->send();
+            if( 200 != $message->getResponseCode() )
+              throw new exc\runtime( 'Unable to fetch consent info from Mastodon', __METHOD__ );
+            $consent_info = json_decode( $message->getBody() );
+            
+            // get the participant's alternate contact information
+            $request->setUrl( $base_url.'participant/list_alternate' );
+            $request->setMethod( \HttpRequest::METH_GET );
+            $request->setQueryData( array( 'uid' => $db_participant->uid ) );
+            $message = $request->send();
+            if( 200 != $message->getResponseCode() )
+              throw new exc\runtime( 'Unable to fetch alternate info from Mastodon', __METHOD__ );
+            $alternate_info = json_decode( $message->getBody() );
+            
+            // email address
+            $sets .= sprintf( ', email = %s', database::format_string(
+              $participant_info->data->email ) );
+            
+            // address
+            $sets .= sprintf( ', attribute_1 = %s', database::format_string(
+              $participant_info->data->street ) );
+            
+            // city
+            $sets .= sprintf( ', attribute_2 = %s', database::format_string(
+              $participant_info->data->city ) );
+            
+            // province
+            $sets .= sprintf( ', attribute_3 = %s', database::format_string(
+              $participant_info->data->region ) );
+            
+            // postcode
+            $sets .= sprintf( ', attribute_4 = %s', database::format_string(
+              $participant_info->data->postcode ) );
+            
+            // age
+            $dob = util::get_datetime_object( $participant_info->data->date_of_birth );
+            $sets .= sprintf( ', attribute_5 = %s', database::format_string(
+              util::get_interval( $dob )->y ) );
+            
+            // written consent received
+            $written_consent = false;
+            foreach( $consent_info->data as $consent )
+            {
+              if( 'written' == substr( $consent->event, 0, 7 ) )
+              {
+                $written_consent = true;
+                break;
+              }
+            }
+            $sets .= sprintf( ', attribute_6 = %s', database::format_string(
+              $written_consent ) );
+            
+            // consented to provide HIN
+            $sets .= sprintf( ', attribute_7 = %s', database::format_string(
+              true == $participant_info->data->hin_access ) );
+            
+            // operator's firstname
+            $sets .= sprintf( ', attribute_8 = %s', database::format_string(
+              $db_user->first_name ) );
+            
+            // operator's lastname
+            $sets .= sprintf( ', attribute_9 = %s', database::format_string(
+              $db_user->last_name ) );
+            
+            // previous CCHS contact date
+            $sets .= sprintf( ', attribute_10 = %s', database::format_string(
+              $participant_info->data->prior_contact_date ) );
+            
+            // number of alternate contacts
+            $number_of_alts = count( $alternate_info->data );
+            $sets .= sprintf( ', attribute_11 = %s', database::format_string(
+              $number_of_alts ) );
+            
+            if( 0 < $number_of_alts )
+            {
+              // alternate's firstname
+              $sets .= sprintf( ', attribute_12 = %s', database::format_string(
+                $alternate_info->data[0]->first_name ) );
+              
+              // alternate's lastname
+              $sets .= sprintf( ', attribute_13 = %s', database::format_string(
+                $alternate_info->data[0]->last_name ) );
+              
+              // alternate's phone
+              $sets .= sprintf( ', attribute_14 = %s', database::format_string(
+                $alternate_info->data[0]->phone ) );
+            }
+          }
+          
           limesurvey\record::db()->execute(
-            sprintf( 'INSERT INTO %s SET token = %s',
-                   $tokens_table,
-                   database::format_string( $token ) ) );
+            sprintf( 'INSERT INTO %s SET %s', $tokens_table, $sets ) );
           $this->current_phase = $db_phase;
           break;
         }
@@ -83,7 +223,6 @@ class assignment extends has_note
 
       if( is_null( $this->current_phase ) )
       { // all phases are complete
-        $db_interview = $this->get_interview();
         $db_interview->completed = true;
         $db_interview->save();
       }
--- aux/survey_runtime.js
+++ aux/survey_runtime.js
@@ -1,29 +1,7 @@
 // The following was added in order to integrate with Sabretooth
 jQuery(document).ready( function() {
-  var sabretooth_url = "../sabretooth/";
   $question = $( "div[id^=question]" )
   if( $question && $question.html() ) {
-    jQuery.getJSON(
-      sabretooth_url + "self/primary",
-      function( json ) {
-        if( json.success ) {
-          $question.html( $question.html().replace( /{operator:first_name}/gi, json.data.first_name ) );
-          $question.html( $question.html().replace( /{operator:last_name}/gi, json.data.last_name ) );
-        }
-      } );
-    jQuery.getJSON(
-      sabretooth_url + "participant/primary?id=assignment",
-      function( json ) {
-        if( json.success ) {
-          $question.html( $question.html().replace( /{participant:first_name}/gi, json.data.first_name ) );
-          $question.html( $question.html().replace( /{participant:last_name}/gi, json.data.last_name ) );
-          $question.html( $question.html().replace( /{participant:street}/gi, json.data.street ) );
-          $question.html( $question.html().replace( /{participant:city}/gi, json.data.city ) );
-          $question.html( $question.html().replace( /{participant:region}/gi, json.data.region ) );
-          $question.html( $question.html().replace( /{participant:postcode}/gi, json.data.postcode ) );
-        }
-      } );
-    
     var now = new Date();
     var hour = now.getHours();
     var period = hour < 12 ? "morning" : hour < 17 ? "afternoon" : "evening";
