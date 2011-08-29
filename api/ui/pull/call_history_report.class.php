943ff32688fba6b8f97180cb900e71f4a8ca4cd6
Patrick Emond <emondpd@mcmaster.ca>
2011-08-29 11:01:59 -0400

    0000120: Problem with Generating Call History R...
    Fixed bug where not using an array reference when calling array_shift.
    Also fixed other minor issues.

--- api/ui/pull/call_history_report.class.php
+++ api/ui/pull/call_history_report.class.php
@@ -38,7 +38,7 @@ class call_history_report extends base_report
   {
     $restrict_site_id = $this->get_argument( 'restrict_site_id', 0 );
     
-    $title = 'Call Attempts Report';
+    $title = 'Call History Report';
     if( $restrict_site_id )
     {
       $db_site = new db\site( $restrict_site_id );
@@ -73,9 +73,6 @@ class call_history_report extends base_report
           substr( $db_phone_call->end_datetime,
                   strpos( $db_phone_call->end_datetime, ' ' ) + 1 ),
           $db_phone_call->status );
-
-        // remove the site if we are restricting the report
-        if( $restrict_site_id ) array_shift( current( $contents ) );
       }
     }
     
@@ -90,7 +87,11 @@ class call_history_report extends base_report
       'Call Result' );
     
     // remove the site if we are restricting the report
-    if( $restrict_site_id ) array_shift( $header );
+    if( $restrict_site_id )
+    {
+      array_shift( $header );
+      foreach( $contents as $index => $content ) array_shift( $contents[$index] );
+    }
 
     $this->add_table( NULL, $header, $contents, NULL );
 
