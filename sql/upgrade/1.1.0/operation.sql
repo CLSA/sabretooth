5ea31d23636d4ad9ee274eb5bde7d594f9427f7e
Patrick Emond <emondpd@mcmaster.ca>
2012-01-20 14:39:46 -0500

    Adding missing IGNORE statements in patch

--- sql/upgrade/1.1.0/operation.sql
+++ sql/upgrade/1.1.0/operation.sql
@@ -3,7 +3,7 @@ INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "widget", "recording", "list", true, "Lists recordings." );
 
 -- Adding the new tree functionality
-INSERT INTO operation( type, subject, name, restricted, description )
+INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "pull", "participant", "tree", true, "Returns the number of participants for every node of the participant tree." );
 
 -- Adding the source survey functionality
--- sql/upgrade/1.1.0/role_has_operation.sql
+++ sql/upgrade/1.1.0/role_has_operation.sql
@@ -15,11 +15,11 @@ AND operation_id = ( SELECT id FROM operation WHERE
       type = "widget" AND subject = "participant" AND name = "tree" );
 
 -- add the new tree functionality
-INSERT INTO role_has_operation
+INSERT IGNORE INTO role_has_operation
 SET role_id = ( SELECT id FROM role WHERE name = "administrator" ),
     operation_id = ( SELECT id FROM operation WHERE
       type = "pull" AND subject = "participant" AND name = "tree" );
-INSERT INTO role_has_operation
+INSERT IGNORE INTO role_has_operation
 SET role_id = ( SELECT id FROM role WHERE name = "supervisor" ),
     operation_id = ( SELECT id FROM operation WHERE
       type = "pull" AND subject = "participant" AND name = "tree" );
