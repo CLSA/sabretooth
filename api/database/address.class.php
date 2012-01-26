3a5cf3e57712d4743a1dd65277b5eb7c91d7f37d
Patrick Emond <emondpd@mcmaster.ca>
2012-01-26 16:42:41 -0500

    Fixing get_time_diff() and adding is_valid() to address record

--- api/database/address.class.php
+++ api/database/address.class.php
@@ -25,15 +25,12 @@ class address extends \cenozo\database\has_rank
    */
   public function get_time_diff()
   {
-    // check the primary key value
-    if( is_null( $this->id ) )
-    {
-      log::warning( 'Tried to query address with no id.' );
-      return NULL;
-    }
-    
     // get the user's timezone differential from UTC
     $user_offset = util::get_datetime_object()->getOffset() / 3600;
+
+    // determine if we are currently under daylight savings
+    $summer_offset = util::get_datetime_object( '2000-07-01' )->getOffset() / 3600;
+    $under_daylight_savings = $user_offset == $summer_offset;
 
     // if we have a postal code, then look up the postal code database (if it is available)
     if( !is_null( $this->postcode ) &&
@@ -54,13 +51,58 @@ class address extends \cenozo\database\has_rank
       $row = static::db()->get_row( $sql );
       if( 0 < count( $row ) )
       {
-        $offset = $row['timezone_offset'] + ( $row['daylight_savings'] ? 1 : 0 );
+        $offset = $row['timezone_offset'];
+        if( $under_daylight_savings &&  $row['daylight_savings'] ) $offset += 1;
         return $offset - $user_offset;
       }
     }
 
     // if we get here then there is no way to get the time difference
     return NULL;
+  }
+
+  /**
+   * Determines if the address is valid by making sure all address-based manditory fields
+   * are filled and checking for postcode-region mismatches.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @return boolean
+   * @access public
+   */
+  public function is_valid()
+  {
+    // make sure all mandatory address-based fields are filled in
+    if( is_null( $this->address1 ) ||
+        is_null( $this->city ) ||
+        is_null( $this->region_id ) ||
+        is_null( $this->postcode ) ) return false;
+
+    // look up the postal code database (if it is available)
+    if( !is_null( $this->postcode ) &&
+        static::db()->get_one(
+          'SELECT COUNT(*) '.
+          'FROM information_schema.schemata '.
+          'WHERE schema_name = "address_info"' ) )
+    {
+      $postcode = 6 == strlen( $this->postcode )
+                   ? substr( $this->postcode, 0, 3 ).' '.substr( $this->postcode, -3 )
+                   : $this->postcode;
+      $postcode = strtoupper( $postcode );
+
+      $sql = sprintf( 'SELECT st '.
+                      'FROM address_info.zcu '.
+                      'WHERE zip = "%s"',
+                      $postcode );
+      $abbreviation = static::db()->get_one( $sql );
+      if( is_null( $abbreviation ) ) return false;
+
+      // create the region record directly (since this record may not exist in the database)
+      $db_region = lib::create( 'database\region', $this->region_id );
+
+      // check for postcode/province mismatches
+      if( $db_region->abbreviation != $abbreviation ) return false;
+    }
+    
+    return true;
   }
 
   /**
