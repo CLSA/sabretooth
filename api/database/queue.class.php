14a01adb9b9cbc8731527c6956c0980305103935
Patrick Emond <emondpd@mcmaster.ca>
2012-02-27 16:33:00 -0500

    Queue counting cache is now site-specific

--- api/database/queue.class.php
+++ api/database/queue.class.php
@@ -163,12 +163,14 @@ class queue extends \cenozo\database\record
    */
   public function get_participant_count( $modifier = NULL, $use_cache = true )
   {
+    $site_id = is_null( $this->db_site ) ? 0 : $this->db_site->id;
     $qnaire_id = !$this->qnaire_specific || is_null( $this->db_qnaire )
                ? 0 : $this->db_qnaire->id;
     if( $use_cache &&
         array_key_exists( $this->name, self::$participant_count_cache ) &&
-        array_key_exists( $qnaire_id, self::$participant_count_cache[$this->name] ) )
-      return self::$participant_count_cache[$this->name][$qnaire_id];
+        array_key_exists( $qnaire_id, self::$participant_count_cache[$this->name] ) &&
+        array_key_exists( $site_id, self::$participant_count_cache[$this->name][$qnaire_id] ) )
+      return self::$participant_count_cache[$this->name][$qnaire_id][$site_id];
 
     if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
 
@@ -177,16 +179,18 @@ class queue extends \cenozo\database\record
     
     if( !array_key_exists( $this->name, self::$participant_count_cache ) )
       self::$participant_count_cache[$this->name] = array();
-    self::$participant_count_cache[$this->name][$qnaire_id] = (integer) static::db()->get_one(
-      sprintf( '%s %s',
-               $this->get_sql( 'COUNT( DISTINCT participant.id )' ),
-               $modifier->get_sql( true ) ) );
+    if( !array_key_exists( $qnaire_id, self::$participant_count_cache[$this->name] ) )
+      self::$participant_count_cache[$this->name][$qnaire_id] = array();
+    self::$participant_count_cache[$this->name][$qnaire_id][$site_id] =
+      (integer) static::db()->get_one( sprintf( '%s %s',
+        $this->get_sql( 'COUNT( DISTINCT participant.id )' ),
+        $modifier->get_sql( true ) ) );
 
     // if the value is 0 then update all child counts with 0 to save processing time
-    if( 0 == self::$participant_count_cache[$this->name][$qnaire_id] )
-      static::set_child_count_cache_to_zero( $this );
+    if( 0 == self::$participant_count_cache[$this->name][$qnaire_id][$site_id] )
+      static::set_child_count_cache_to_zero( $this, $qnaire_id, $site_id );
 
-    return self::$participant_count_cache[$this->name][$qnaire_id];
+    return self::$participant_count_cache[$this->name][$qnaire_id][$site_id];
   }
 
   /**
@@ -194,21 +198,23 @@ class queue extends \cenozo\database\record
    * 
    * @author Patrick Emond <emondpd@mcmaster.ca>
    * @param database\queue $db_queue
+   * @param int $qnaire_id The qnaire id being processed.
+   * @param int $site_id The site id being processed.
    * @static
    * @access private
    */
-  private static function set_child_count_cache_to_zero( $db_queue )
+  private static function set_child_count_cache_to_zero( $db_queue, $qnaire_id, $site_id )
   {
-    $qnaire_id = !$db_queue->qnaire_specific || is_null( $db_queue->db_qnaire )
-               ? 0 : $db_queue->db_qnaire->id;
     $queue_mod = lib::create( 'database\modifier' );
     $queue_mod->where( 'parent_queue_id', '=', $db_queue->id );
     foreach( static::select( $queue_mod ) as $db_child_queue )
     {
       if( !array_key_exists( $db_child_queue->name, self::$participant_count_cache ) )
         self::$participant_count_cache[$db_child_queue->name] = array();
-      self::$participant_count_cache[$db_child_queue->name][$qnaire_id] = 0;
-      self::set_child_count_cache_to_zero( $db_child_queue );
+      if( !array_key_exists( $qnaire_id, self::$participant_count_cache[$db_child_queue->name] ) )
+        self::$participant_count_cache[$db_child_queue->name][$qnaire_id] = array();
+      self::$participant_count_cache[$db_child_queue->name][$qnaire_id][$site_id] = 0;
+      self::set_child_count_cache_to_zero( $db_child_queue, $qnaire_id, $site_id );
     }
   }
 
