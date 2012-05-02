db975f6761d825f8799ce7c1b9ef8830fc40384b
Patrick Emond <emondpd@mcmaster.ca>
2012-05-02 13:22:05 -0400

    Adding cohort to site names in access delete push

--- api/ui/push/access_delete.class.php
+++ api/ui/push/access_delete.class.php
@@ -29,5 +29,19 @@ class access_delete extends \cenozo\ui\push\access_delete
     $this->set_machine_request_enabled( true );
     $this->set_machine_request_url( MASTODON_URL );
   }
+
+  /**
+   * Override the parent method to add the cohort to the site key.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param array $args An argument list, usually those passed to the push operation.
+   * @return array
+   * @access protected
+   */
+  protected function convert_to_noid( $args )
+  {
+    $args = parent::convert_to_noid( $args );
+    $args['noid']['site']['cohort'] = 'tracking';
+    return $args;
+  }
 }
 ?>
