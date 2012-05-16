85414fc04e250e663558c2a0ead7605a7850caab
Patrick Emond <emondpd@mcmaster.ca>
2012-05-16 14:22:28 -0400

    Implementing availability to new operation format

--- api/ui/push/availability_new.class.php
+++ api/ui/push/availability_new.class.php
@@ -27,6 +27,13 @@ class availability_new extends base_new
   public function __construct( $args )
   {
     parent::__construct( 'availability', $args );
+  }
+
+  // TODO: document
+  protected function prepare()
+  {
+    parent::prepare();
+
     $this->set_machine_request_enabled( true );
     $this->set_machine_request_url( MASTODON_URL );
   }
