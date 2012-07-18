d27ba7fc297063bdb1e562d5bc2b24b190531d99
Patrick Emond <emondpd@mcmaster.ca>
2012-07-18 12:15:11 -0400

    0000382: Age availability queue/category

--- api/ui/widget/participant_view.class.php
+++ api/ui/widget/participant_view.class.php
@@ -51,6 +51,8 @@ class participant_view extends \cenozo\ui\widget\base_view
     $this->add_item( 'status', 'enum', 'Condition' );
     $this->add_item( 'default_site', 'constant', 'Default Site' );
     $this->add_item( 'site_id', 'enum', 'Prefered Site' );
+    $this->add_item( 'gender', 'enum', 'Gender' );
+    $this->add_item( 'date_of_birth', 'date', 'Date of Birth' );
     $this->add_item( 'prior_contact_date', 'constant', 'Prior Contact Date' );
     $this->add_item( 'current_qnaire_name', 'constant', 'Current Questionnaire' );
     $this->add_item( 'start_qnaire_date', 'constant', 'Delay Questionnaire Until' );
@@ -113,6 +115,7 @@ class participant_view extends \cenozo\ui\widget\base_view
     $participant_class_name = lib::get_class_name( 'database\participant' );
     $site_class_name = lib::get_class_name( 'database\site' );
     $operation_class_name = lib::get_class_name( 'database\operation' );
+    $record = $this->get_record();
 
     // create enum arrays
     $languages = $participant_class_name::get_enum_values( 'language' );
@@ -124,36 +127,40 @@ class participant_view extends \cenozo\ui\widget\base_view
     $site_mod->order( 'name' );
     foreach( $site_class_name::select( $site_mod ) as $db_site )
       $sites[$db_site->id] = $db_site->name;
-    $db_site = $this->get_record()->get_site();
+    $db_site = $record->get_site();
     $site_id = is_null( $db_site ) ? '' : $db_site->id;
+    $genders = $participant_class_name::get_enum_values( 'gender' );
+    $genders = array_combine( $genders, $genders );
     
-    $start_qnaire_date = $this->get_record()->start_qnaire_date;
-    if( is_null( $this->get_record()->current_qnaire_id ) )
+    $start_qnaire_date = $record->start_qnaire_date;
+    if( is_null( $record->current_qnaire_id ) )
     {
       $current_qnaire_name = '(none)';
       $start_qnaire_date = '(not applicable)';
     }
     else
     {
-      $db_current_qnaire = lib::create( 'database\qnaire', $this->get_record()->current_qnaire_id );
+      $db_current_qnaire = lib::create( 'database\qnaire', $record->current_qnaire_id );
       $current_qnaire_name = $db_current_qnaire->name;
       $start_qnaire_date = util::get_formatted_date( $start_qnaire_date, 'immediately' );
     }
 
-    $db_default_site = $this->get_record()->get_default_site();
+    $db_default_site = $record->get_default_site();
     $default_site = is_null( $db_default_site ) ? 'None' : $db_default_site->name;
 
     // set the view's items
-    $this->set_item( 'active', $this->get_record()->active, true );
-    $this->set_item( 'uid', $this->get_record()->uid );
-    $this->set_item( 'source', $this->get_record()->get_source()->name );
-    $this->set_item( 'first_name', $this->get_record()->first_name );
-    $this->set_item( 'last_name', $this->get_record()->last_name );
-    $this->set_item( 'language', $this->get_record()->language, false, $languages );
-    $this->set_item( 'status', $this->get_record()->status, false, $statuses );
+    $this->set_item( 'active', $record->active, true );
+    $this->set_item( 'uid', $record->uid );
+    $this->set_item( 'source', $record->get_source()->name );
+    $this->set_item( 'first_name', $record->first_name );
+    $this->set_item( 'last_name', $record->last_name );
+    $this->set_item( 'language', $record->language, false, $languages );
+    $this->set_item( 'status', $record->status, false, $statuses );
     $this->set_item( 'default_site', $default_site );
     $this->set_item( 'site_id', $site_id, false, $sites );
-    $this->set_item( 'prior_contact_date', $this->get_record()->prior_contact_date );
+    $this->set_item( 'gender', $record->gender, true, $genders );
+    $this->set_item( 'date_of_birth', $record->date_of_birth );
+    $this->set_item( 'prior_contact_date', $record->prior_contact_date );
     $this->set_item( 'current_qnaire_name', $current_qnaire_name );
     $this->set_item( 'start_qnaire_date', $start_qnaire_date );
 
