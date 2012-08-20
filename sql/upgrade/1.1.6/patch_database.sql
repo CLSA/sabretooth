269ec456c96393c4360b45e5acba987e238f0bab
Patrick Emond <emondpd@mcmaster.ca>
2012-08-20 15:59:22 -0400

    Moving patch for 1.0.4 to 1.0.3 (see previous commit)

--- sql/upgrade/1.1.6/patch_database.sql
+++ sql/upgrade/1.1.6/patch_database.sql
@@ -2,3 +2,10 @@
 
 SOURCE operation.sql
 SOURCE role_has_operation.sql
+SOURCE participant_last_assignment.sql
+SOURCE interview_phone_call_status_count.sql
+SOURCE participant_phone_call_status_count.sql
+SOURCE participant_last_appointment.sql
+SOURCE participant_last_consent.sql
+SOURCE quota.sql
+SOURCE queue.sql
