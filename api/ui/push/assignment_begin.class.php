817d11df968ab2bc51eb63ae01a6d5a5e02c1f23
Patrick Emond <emondpd@mcmaster.ca>
2012-08-21 16:40:05 -0400

    Ordering queues by age>65 then source

--- api/ui/push/assignment_begin.class.php
+++ api/ui/push/assignment_begin.class.php
@@ -102,7 +102,9 @@ class assignment_begin extends \cenozo\ui\push
         {
           $participant_mod = lib::create( 'database\modifier' );
           // on a weekday sort the queue by age, the order defined by the reverse sort time setting
-          if( $weekday ) $participant_mod->order( 'date_of_birth', $age_desc );
+          if( $weekday ) $participant_mod->order(
+            'DATEDIFF( DATE( NOW() ), date_of_birth ) < 65 * 365', $age_desc );
+          $participant_mod->order( 'source_id' );
           $participant_mod->limit( 1 );
 
           $db_queue->set_site( $session->get_site() );
