9a851f1f4fa72299776eb3029e8b3ef5187eb81a
Patrick Emond <emondpd@mcmaster.ca>
2012-10-16 13:23:59 -0400

    Bug in voip_manager: Using SERVER_NAME instead of HTTP_HOST since the second includes port

--- api/business/voip_manager.class.php
+++ api/business/voip_manager.class.php
@@ -25,7 +25,7 @@ class voip_manager extends \cenozo\singleton
   protected function __construct()
   {
     $voip_host = lib::create( 'business\session' )->get_site()->voip_host;
-    if( $_SERVER['HTTP_HOST'] == $voip_host ) $voip_host = 'localhost';
+    if( $_SERVER['SERVER_NAME'] == $voip_host ) $voip_host = 'localhost';
     $setting_manager = lib::create( 'business\setting_manager' );
     $this->enabled = true === $setting_manager->get_setting( 'voip', 'enabled' );
     $this->url = sprintf( $setting_manager->get_setting( 'voip', 'url' ), $voip_host );
