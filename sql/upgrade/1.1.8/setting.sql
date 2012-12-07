deb1589e1e594ef46a01819e0348285889336847
Patrick Emond <emondpd@mcmaster.ca>
2012-12-07 12:19:09 -0500

    Fixing setting name (scheduled call ready => assignable callback)

--- sql/upgrade/1.1.8/setting.sql
+++ sql/upgrade/1.1.8/setting.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO setting( category, name, type, value, description )
-VALUES( "queue state", "scheduled call ready", "boolean", "true",
-       "Whether to assign participants from the \"scheduled call ready\" queue." );
+VALUES( "queue state", "assignable callback", "boolean", "true",
+       "Whether to assign participants from the \"assignable callback\" queue." );
 
 INSERT IGNORE INTO setting( category, name, type, value, description )
 VALUES( "callback", "call pre-window", "integer", "5",
