6f834cf30cc96c9be5dbc12a976a545afcbed3b9
Patrick Emond <emondpd@mcmaster.ca>
2013-01-03 16:47:43 -0500

    Adding charts as an operation type
    - charts are included in their own category in the menu
    - added new "activity" chart which displays server usage measured in processing time

--- sql/upgrade/1.1.9/role_has_operation.sql
+++ sql/upgrade/1.1.9/role_has_operation.sql
@@ -10,3 +10,7 @@ INSERT IGNORE INTO role_has_operation
 SET role_id = ( SELECT id FROM role WHERE name = "administrator" ),
     operation_id = ( SELECT id FROM operation WHERE
       type = "pull" AND subject = "timing" AND name = "report" );
+INSERT IGNORE INTO role_has_operation
+SET role_id = ( SELECT id FROM role WHERE name = "administrator" ),
+    operation_id = ( SELECT id FROM operation WHERE
+      type = "widget" AND subject = "activity" AND name = "chart" );
