7f93bf5ba7628e61a0a9be9add48d87752427c51
Patrick Emond <emondpd@mcmaster.ca>
2013-02-08 16:19:06 -0500

    Making sure that other services never show in lists

---
+++ api/database/cohort.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * cohort.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace sabretooth\database;
+use cenozo\lib, cenozo\log, sabretooth\util;
+
+/**
+ * cohort: record
+ */
+class cohort extends \cenozo\database\cohort
+{
+  /**
+   * Extend parent method by restricting selection to records belonging to this service only
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param database\modifier $modifier Modifications to the selection.
+   * @param boolean $count If true the total number of records instead of a list
+   * @access public
+   * @static
+   */
+  public static function select( $modifier = NULL, $count = false )
+  {
+    // make sure to only include cohorts belonging to this application
+    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
+    $modifier->where( 'service_has_cohort.service_id', '=',
+                      lib::create( 'business\session' )->get_service()->id );
+    return parent::select( $modifier, $count );
+  }
+
+  /**
+   * Make sure to only include cohorts which this service has access to.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param database\modifier $modifier Modifications to the list.
+   * @return array( database\cohort )
+   * @access public
+   */
+  public function get_record_list( 
+    $record_type, $modifier = NULL, $inverted = false, $count = false )
+  { 
+    if( 'service' == $record_type )
+    {
+      if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
+      $modifier->where( 'service_has_cohort.service_id', '=',
+                        lib::create( 'business\session' )->get_service()->id );
+    }                   
+    return parent::get_record_list( $record_type, $modifier, $inverted, $count );
+  }
+}
---
+++ api/database/system_message.class.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * system_message.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace sabretooth\database;
+use cenozo\lib, cenozo\log, sabretooth\util;
+
+/**
+ * system_message: record
+ */
+class system_message extends \cenozo\database\system_message
+{
+  /**
+   * Extend parent method by restricting selection to records belonging to this service only
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param database\modifier $modifier Modifications to the selection.
+   * @param boolean $count If true the total number of records instead of a list
+   * @access public
+   * @static
+   */
+  public static function select( $modifier = NULL, $count = false )
+  {
+    // make sure to only include system_messages belonging to this application
+    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
+    $modifier->where_bracket( true );
+    $modifier->where( 'service_id', '=', lib::create( 'business\session' )->get_service()->id );
+    $modifier->or_where( 'service_id', '=', NULL );
+    $modifier->where_bracket( false );
+    return parent::select( $modifier, $count );
+  }
+}
