0403f787cfb66d1ba6514e0a1f7bd1cd3c495033
Patrick Emond <emondpd@mcmaster.ca>
2013-02-18 01:38:37 -0500

    Putting recordings into sub-directories

--- api/database/recording.class.php
+++ api/database/recording.class.php
@@ -30,8 +30,10 @@ class recording extends \cenozo\database\record
       return NULL;
     }
     
-    return sprintf( 1 == $this->rank ? 'monitor/%d_%d-out.wav' : 'monitor/%d_%d-%d-out.wav',
-                    $this->interview_id,
+    $padded = str_pad( $this->interview_id, 7, '0', STR_PAD_LEFT );
+    $base = sprintf( 'monitor/%s/%s/%s', substr( $padded, 0, 3 ), substr( $padded, 3, 2 ), substr( $padded, 5 ) );
+    return sprintf( 1 == $this->rank ? '%s_%d-out.wav' : '%s_%d-%d-out.wav',
+                    $base,
                     is_null( $this->assignment_id ) ? 0 : $this->assignment_id,
                     $this->rank - 1 );
   }
