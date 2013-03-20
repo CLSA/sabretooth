09138cc34e198332533bfa9451ecdafe99732314
Patrick Emond <emondpd@mcmaster.ca>
2013-03-20 13:52:17 -0400

    Allowing admins on mastodon access to hin editing only

--- sql/roles.sql
+++ sql/roles.sql
@@ -624,11 +624,6 @@ AND role.name IN( "administrator", "operator", "supervisor" );
 
 INSERT INTO role_has_operation( role_id, operation_id )
 SELECT role.id, operation.id FROM cenozo.role, operation
-WHERE type = "widget" AND subject = "participant" AND operation.name = "hin"
-AND role.name IN( "administrator", "operator", "supervisor" );
-
-INSERT INTO role_has_operation( role_id, operation_id )
-SELECT role.id, operation.id FROM cenozo.role, operation
 WHERE type = "pull" AND subject = "participant" AND operation.name = "list"
 AND role.name IN( "administrator", "supervisor" );
 
