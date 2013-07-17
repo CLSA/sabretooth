021e03e0370c4ce30603a3eade7a6e1655ad3867
Patrick Emond <emondpd@mcmaster.ca>
2013-07-17 14:49:10 -0400

    Adding date stamped and form code to contact forms
    Contact forms may now be date-stamped.  If this date is recorded during
    data entry then a "consent to contact stamped" event will be added to
    the participant (if the event exists).  Additionally, a new "grouping"
    column has been added to the participant table which is defined by the
    code selected during the data entry phase.

--- sql/upgrade/1.2.1/participant.sql
+++ sql/upgrade/1.2.1/participant.sql
@@ -27,7 +27,7 @@ CREATE PROCEDURE patch_participant()
       DEALLOCATE PREPARE statement;
     END IF;
 
-    SELECT "Adding new participant.withdraw_letter column" AS "";
+    SELECT "Adding new participant.withdraw_letter and participant.grouping columns" AS "";
     SET @test = (
       SELECT COUNT(*)
       FROM information_schema.COLUMNS
@@ -38,6 +38,31 @@ CREATE PROCEDURE patch_participant()
       SET @sql = CONCAT(
         "ALTER TABLE ", @cenozo, ".participant ",
         "ADD COLUMN withdraw_letter CHAR(1) NULL DEFAULT NULL" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+    END IF;
+
+    SET @test = (
+      SELECT COUNT(*)
+      FROM information_schema.COLUMNS
+      WHERE TABLE_SCHEMA = @cenozo
+      AND TABLE_NAME = "participant"
+      AND COLUMN_NAME = "grouping" );
+    IF @test = 0 THEN
+      SET @sql = CONCAT(
+        "ALTER TABLE ", @cenozo, ".participant ",
+        "ADD COLUMN grouping VARCHAR(45) NULL DEFAULT NULL ",
+        "AFTER cohort_id" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+      -- fill in the grouping based on cohort
+      SET @sql = CONCAT(
+        "UPDATE ", @cenozo, ".participant ",
+        "JOIN ", @cenozo, ".cohort ON participant.cohort_id = cohort.id ",
+        "SET grouping = UPPER( SUBSTR( cohort.name, 1, 1 ) ) " );
       PREPARE statement FROM @sql;
       EXECUTE statement;
       DEALLOCATE PREPARE statement;
