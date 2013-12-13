e51e0987517e1671b9f2a8018615fda0b5a70398
Patrick Emond <emondpd@mcmaster.ca>
2013-12-13 14:33:26 -0500

    Adjusting how age groups are shown in the pre-recruitment widget

--- api/ui/widget/prerecruit_select.class.php
+++ api/ui/widget/prerecruit_select.class.php
@@ -81,12 +81,13 @@ class prerecruit_select extends \cenozo\ui\widget
       $quota_list[] = array(
         'id' => $db_quota->id,
         'gender' => $db_quota->gender,
-        'age_group' => $db_age_group->to_string(),
+        'age_group' => sprintf( '%d to %d', $db_age_group->lower, $db_age_group->upper - 1 ),
         'total' => is_null( $db_prerecruit ) ? 0 : $db_prerecruit->total );
 
       if( !is_null( $db_prerecruit ) && 0 < $db_prerecruit->selected )
       {
-        $selection = sprintf( '%s %s', $db_quota->gender, $db_age_group->to_string() );
+        $selection =
+          sprintf( '%s %d to %d', $db_quota->gender, $db_age_group->lower, $db_age_group->upper - 1 );
         $selected_index = $db_prerecruit->selected;
         $total = $db_prerecruit->total;
       }
