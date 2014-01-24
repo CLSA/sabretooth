1ec0f6ab9da7c843d5b68025a4f530984dfd35fe
Patrick Emond <emondpd@mcmaster.ca>
2014-01-24 13:39:47 -0500

    Removing defunct sql upgrade file

--- sql/upgrade/1.3.0/role_has_operation2.sql
+++
@@ -1,3 +0,0 @@
-SELECT "Removing queue restriction operations from roles" AS "";
-DELETE FROM role_has_operation
-WHERE operation_id IN ( SELECT id FROM operation WHERE subject = "queue_restriction" );
