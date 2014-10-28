169b76d72561772da2d3079bac98709e2f6d7000
Patrick Emond <emondpd@mcmaster.ca>
2014-10-28 15:57:47 -0400

    Adding qnaire start date as parameter to participant report

--- api/ui/pull/participant_report.class.php
+++ api/ui/pull/participant_report.class.php
@@ -26,8 +26,13 @@ class participant_report extends \cenozo\ui\pull\participant_report
   {
     parent::prepare();
 
+    $database_class_name = lib::get_class_name( 'database\database' );
+
     // get the report arguments
     $last_call_result = $this->get_argument( 'last_call_result' );
+    $qnaire_id = $this->get_argument( 'qnaire_id' );
+    $start_date = $this->get_argument( 'qnaire_start_date' );
+    $end_date = $this->get_argument( 'qnaire_end_date' );
 
     if( 'any' != $last_call_result )
     {
@@ -43,6 +48,28 @@ class participant_report extends \cenozo\ui\pull\participant_report
 
       $this->modifier->where(
         'phone_call.status', '=', $last_call_result );
+    }
+
+    if( $qnaire_id || $start_date || $end_date )
+    {
+      $this->sql_tables .=
+        'JOIN queue_has_participant '.
+        'ON participant.id = queue_has_participant.participant_id '.
+        'AND queue_has_participant.start_qnaire_date IS NOT NULL ';
+
+      if( $qnaire_id )
+        $this->sql_tables .= sprintf(
+          'AND queue_has_participant.qnaire_id = %s ',
+          $database_class_name::format_string( $qnaire_id ) );
+
+      $this->sql_tables .=
+        'JOIN queue ON queue_has_participant.queue_id = queue.id '.
+        'AND queue.name = "qnaire" ';
+
+      if( $start_date )
+        $this->modifier->where( 'queue_has_participant.start_qnaire_date', '>=', $start_date );
+      if( $end_date )
+        $this->modifier->where( 'queue_has_participant.start_qnaire_date', '<=', $end_date );
     }
   }
 }
--- api/ui/widget/participant_report.class.php
+++ api/ui/widget/participant_report.class.php
@@ -26,6 +26,11 @@ class participant_report extends \cenozo\ui\widget\participant_report
     parent::prepare();
 
     $this->add_parameter( 'last_call_result', 'enum', 'Last Call Result' );
+
+    $this->add_separator();
+    $this->add_parameter( 'qnaire_id', 'enum', 'Questionnaire' );
+    $this->add_parameter( 'qnaire_start_date', 'date', 'Available Start Date' );
+    $this->add_parameter( 'qnaire_end_date', 'date', 'Available End Date' );
   }
 
   /**
@@ -39,13 +44,22 @@ class participant_report extends \cenozo\ui\widget\participant_report
     parent::setup();
 
     $phone_call_class_name = lib::get_class_name( 'database\phone_call' );
+    $qnaire_class_name = lib::get_class_name( 'database\qnaire' );
 
     // create the enum lists
     $call_results = array( 'any' );
     $call_results =
       array_merge( $call_results, $phone_call_class_name::get_enum_values( 'status' ) );
     $call_results = array_combine( $call_results, $call_results );
+    $qnaire_list = array();
+    $qnaire_mod = lib::create( 'database\modifier' );
+    $qnaire_mod->order( 'name' );
+    foreach( $qnaire_class_name::select( $qnaire_mod ) as $db_qnaire )
+      $qnaire_list[$db_qnaire->id] = $db_qnaire->name;
 
     $this->set_parameter( 'last_call_result', current( $call_results ), true, $call_results );
+    $this->set_parameter( 'qnaire_id', current( $qnaire_list ), false, $qnaire_list );
+    $this->set_parameter( 'qnaire_start_date', NULL, false );
+    $this->set_parameter( 'qnaire_end_date', NULL, false );
   }
 }
