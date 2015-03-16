bec225e0a6874ac2872102a8293ca00db313024e
Patrick Emond <emondpd@mcmaster.ca>
2015-03-16 13:24:48 -0400

    Interface redesign [WIP]

--- web/api/index.php
+++ web/api/index.php
@@ -10,6 +10,6 @@
 // load web-script common code
 require_once '../../settings.ini.php';
 require_once '../../settings.local.ini.php';
-require_once $SETTINGS['path']['CENOZO'].'/app/api.class.php';
-$api = new \cenozo\api();
-$api->execute();
+require_once $SETTINGS['path']['CENOZO'].'/api/bootstrap.class.php';
+$bootstrap = new \cenozo\bootstrap();
+$bootstrap->initialize( 'api' );
