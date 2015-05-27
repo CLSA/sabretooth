36be08608705204141df6c072d4083afb8d6c270
Patrick Emond <emondpd@mcmaster.ca>
2015-05-27 12:27:07 -0400

    Restricting assignments by site

--- api/service/assignment/module.class.php
+++ api/service/assignment/module.class.php
@@ -21,18 +21,30 @@ class module extends \cenozo\service\module
   {
     parent::prepare_read( $select, $modifier );
 
+    $session = lib::create( 'business\session' );
+
+    // restrict to participants in this site (for some roles)
+    if( $select->has_table_columns( 'participant' ) || !$session->get_role()->all_sites )
+    {
+      $modifier->join( 'interview', 'assignment.interview_id', 'interview.id' );
+      $modifier->join( 'participant', 'interview.participant_id', 'participant.id' );
+
+      if( !$session->get_role()->all_sites )
+      {
+        $sub_mod = lib::create( 'database\modifier' );
+        $sub_mod->where( 'participant.id', '=', 'participant_site.participant_id', false );
+        $sub_mod->where( 'participant_site.application_id', '=', $session->get_application()->id );
+
+        $modifier->join_modifier( 'participant_site', $sub_mod );
+        $modifier->where( 'participant_site.site_id', '=', $session->get_site()->id );
+      }
+    }
+
     // add the assignment's last call's status column
     $modifier->cross_join( 'assignment_last_phone_call',
       'assignment.id', 'assignment_last_phone_call.assignment_id' );
     $modifier->cross_join( 'phone_call AS last_phone_call',
       'assignment_last_phone_call.phone_call_id', 'last_phone_call.id' );
     $select->add_table_column( 'last_phone_call', 'status' );
-
-    // join to participant table
-    if( $select->has_table_columns( 'participant' ) )
-    {
-      $modifier->join( 'interview', 'assignment.interview_id', 'interview.id' );
-      $modifier->join( 'participant', 'interview.participant_id', 'participant.id' );
-    }
   }
 }
