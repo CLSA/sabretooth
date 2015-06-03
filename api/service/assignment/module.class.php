57bd35eca0c779b19a57b5929de4c313e5fbbf06
Patrick Emond <emondpd@mcmaster.ca>
2015-06-03 11:46:53 -0400

    Fixing bug in assignment module

--- api/service/assignment/module.class.php
+++ api/service/assignment/module.class.php
@@ -24,20 +24,15 @@ class module extends \cenozo\service\module
     $session = lib::create( 'business\session' );
 
     // restrict to participants in this site (for some roles)
-    if( $select->has_table_columns( 'participant' ) || !$session->get_role()->all_sites )
+    if( !$session->get_role()->all_sites )
+    {
+      $modifier->where( 'assignment.site_id', '=', $session->get_site()->id );
+    }
+
+    if( $select->has_table_columns( 'participant' ) )
     {
       $modifier->join( 'interview', 'assignment.interview_id', 'interview.id' );
       $modifier->join( 'participant', 'interview.participant_id', 'participant.id' );
-
-      if( !$session->get_role()->all_sites )
-      {
-        $sub_mod = lib::create( 'database\modifier' );
-        $sub_mod->where( 'participant.id', '=', 'participant_site.participant_id', false );
-        $sub_mod->where( 'participant_site.application_id', '=', $session->get_application()->id );
-
-        $modifier->join_modifier( 'participant_site', $sub_mod );
-        $modifier->where( 'participant_site.site_id', '=', $session->get_site()->id );
-      }
     }
 
     // add the assignment's last call's status column
