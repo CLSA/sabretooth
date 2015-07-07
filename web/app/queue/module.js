47d89cf298ba59d460e47ae7e05c7fb10295e67e
Patrick Emond <emondpd@mcmaster.ca>
2015-07-07 11:47:53 -0400

    Partial implementation of queue module

--- web/app/queue/module.js
+++ web/app/queue/module.js
@@ -8,21 +8,40 @@ define( {
     pluralPossessive: 'queues\''
   },
   inputList: {
-    // TODO: fill out
+    rank: {
+      title: 'Rank',
+      type: 'rank',
+      constant: true
+    },
+    name: {
+      title: 'Name',
+      type: 'string',
+      constant: true
+    },
+    title: {
+      title: 'Title',
+      type: 'string',
+      constant: true
+    },
+    description: {
+      title: 'Description',
+      type: 'text',
+      constant: true
+    }
   },
   columnList: {
     rank: {
       title: 'Rank',
       type: 'rank'
     },
-    name: { title: 'Name' },
+    name: { title: 'Name' }/*,
     participant_count: {
       title: 'Participants',
       type: 'number'
-    }
+    }*/
   },
   defaultOrder: {
-    column: 'rank',
+    column: 'queue.id',
     reverse: false
   }
 } );
