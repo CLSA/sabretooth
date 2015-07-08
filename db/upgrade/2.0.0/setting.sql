eed3c3550c41d3a914252a968836ca03ff59c493
Patrick Emond <emondpd@mcmaster.ca>
2015-07-08 17:57:55 -0400

    Creating new setting table to hold site settings

--- db/upgrade/2.0.0/setting.sql
+++ db/upgrade/2.0.0/setting.sql
@@ -1,3 +1,96 @@
-SELECT "Removing defunct setting table" AS "";
+DROP PROCEDURE IF EXISTS patch_setting;
+  DELIMITER //
+  CREATE PROCEDURE patch_setting()
+  BEGIN
 
-DROP TABLE IF EXISTS setting;
+    -- determine the @cenozo database name
+    SET @cenozo = (
+      SELECT unique_constraint_schema
+      FROM information_schema.referential_constraints
+      WHERE constraint_schema = DATABASE()
+      AND constraint_name = "fk_queue_state_site_id" );
+
+    SELECT "Replacing existing setting table with new design" AS "";
+
+    SET @test = (
+      SELECT COUNT(*)
+      FROM information_schema.TABLES
+      WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = "old_setting_value" );
+    IF @test = 1 THEN
+
+      DROP TABLE setting;
+
+      SET @sql = CONCAT(
+        "CREATE TABLE setting ( ",
+          "id INT UNSIGNED NOT NULL AUTO_INCREMENT, ",
+          "update_timestamp TIMESTAMP NOT NULL, ",
+          "create_timestamp TIMESTAMP NOT NULL, ",
+          "site_id INT UNSIGNED NOT NULL, ",
+          "survey_without_sip TINYINT(1) NOT NULL DEFAULT 0, ",
+          "calling_start_time TIME NOT NULL DEFAULT '9:00', ",
+          "calling_end_time TIME NOT NULL DEFAULT '21:00', ",
+          "short_appointment INT UNSIGNED NOT NULL DEFAULT 30, ",
+          "long_appointment INT UNSIGNED NOT NULL DEFAULT 60, ",
+          "pre_call_window INT UNSIGNED NOT NULL DEFAULT 5, ",
+          "post_call_window INT UNSIGNED NOT NULL DEFAULT 15, ",
+          "PRIMARY KEY (id), ",
+          "INDEX fk_site_id (site_id ASC), ",
+          "CONSTRAINT fk_setting_site_id ",
+            "FOREIGN KEY (site_id) ",
+            "REFERENCES ", @cenozo, ".site (id) ",
+            "ON DELETE NO ACTION ",
+            "ON UPDATE NO ACTION ) ",
+        "ENGINE = InnoDB" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+      -- insert data from temporary "old_setting_value" table
+      INSERT INTO setting( site_id ) SELECT site_id FROM old_setting_value;
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET survey_without_sip = "true" = old_setting_value.value
+      WHERE old_setting_value.category = "voip"
+      AND old_setting_value.name = "survey without sip";
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET calling_start_time = value
+      WHERE old_setting_value.category = "calling"
+      AND old_setting_value.name = "start time";
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET calling_end_time = value
+      WHERE old_setting_value.category = "calling"
+      AND old_setting_value.name = "end time";
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET short_appointment = value
+      WHERE old_setting_value.category = "appointment"
+      AND old_setting_value.name = "half duration";
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET long_appointment = value
+      WHERE old_setting_value.category = "appointment"
+      AND old_setting_value.name = "full duration";
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET pre_call_window = value
+      WHERE old_setting_value.category = "appointment"
+      AND old_setting_value.name = "call pre-window";
+
+      UPDATE setting JOIN old_setting_value USING( site_id )
+      SET post_call_window = value
+      WHERE old_setting_value.category = "appointment"
+      AND old_setting_value.name = "call post-window";
+
+      DROP TABLE old_setting_value;
+
+    END IF;
+
+  END //
+DELIMITER ;
+
+-- now call the procedure and remove the procedure
+CALL patch_setting();
+DROP PROCEDURE IF EXISTS patch_setting;
--- db/upgrade/2.0.0/setting_value.sql
+++ db/upgrade/2.0.0/setting_value.sql
@@ -1,3 +1,54 @@
-SELECT "Removing defunct setting_value table" AS "";
+DROP PROCEDURE IF EXISTS patch_setting_value;
+  DELIMITER //
+  CREATE PROCEDURE patch_setting_value()
+  BEGIN
 
-DROP TABLE IF EXISTS setting_value;
+    -- determine the @cenozo database name
+    SET @cenozo = (
+      SELECT unique_constraint_schema
+      FROM information_schema.referential_constraints
+      WHERE constraint_schema = DATABASE()
+      AND constraint_name = "fk_queue_state_site_id" );
+
+    -- determine this application's name
+    SET @application = (
+      SELECT REPLACE( DATABASE(),
+                      CONCAT( SUBSTRING( USER(), 1, LOCATE( '@', USER() ) - 1 ), "_" ),
+                      "" ) );
+
+    SELECT "Removing defunct setting_value table" AS "";
+
+    SET @test = (
+      SELECT COUNT(*)
+      FROM information_schema.TABLES
+      WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = "setting_value" );
+    IF @test = 1 THEN
+
+      -- save the setting data to use later
+      SET @sql = CONCAT(
+        "CREATE TABLE old_setting_value AS ",
+        "SELECT setting.category, setting.name, site.id AS site_id, ",
+               "IFNULL( setting_value.value, setting.value ) AS value ",
+        "FROM ", @cenozo, ".application ",
+        "JOIN ", @cenozo, ".site ON application.id = site.application_id ",
+        "CROSS JOIN setting ",
+        "LEFT JOIN setting_value ",
+        "ON setting.id = setting_value.setting_id ",
+        "AND site.id = setting_value.site_id ",
+        "WHERE application.name = '", @application, "' ",
+        "ORDER BY site.id, setting.id" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+      DROP TABLE setting_value;
+
+    END IF;
+
+  END //
+DELIMITER ;
+
+-- now call the procedure and remove the procedure
+CALL patch_setting_value();
+DROP PROCEDURE IF EXISTS patch_setting_value;
