a36d99bab7449a730062997beb7aeb1976281f7c
Patrick Emond <emondpd@mcmaster.ca>
2015-08-26 17:37:39 -0400

    Adding open_phone_call methods to database\user

--- api/database/user.class.php
+++ api/database/user.class.php
@@ -51,4 +51,46 @@ class user extends \cenozo\database\user
       log::warning( sprintf( 'User %d (%s) has more than one open assignment!', $this->id, $this->name ) );
     return 0 < count( $assignment_list ) ? current( $assignment_list ) : NULL;
   }
+
+  /**
+   * TODO: document
+   */
+  function has_open_phone_call()
+  {
+    // check the primary key value
+    if( is_null( $this->id ) )
+    {
+      log::warning( 'Tried to determine if user with no id has an open phone call.' );
+      return NULL;
+    }
+
+    $assignment_mod = lib::create( 'database\modifier' );
+    $assignment_mod->join( 'phone_call', 'assignment.id', 'phone_call.assignment_id' );
+    $assignment_mod->where( 'phone_call.end_datetime', '=', NULL );
+    return 0 < $this->get_assignment_count( $assignment_mod );
+  }
+
+  /**
+   * TODO: document
+   */
+  function get_open_phone_call()
+  {
+    // check the primary key value
+    if( is_null( $this->id ) )
+    {
+      log::warning( 'Tried to get open phone_call from user with no id.' );
+      return NULL;
+    }
+
+    $phone_call_class_name = lib::get_class_name( 'database\phone_call' );
+    $phone_call_mod = lib::create( 'database\modifier' );
+    $phone_call_mod->join( 'assignment', 'phone_call.assignment_id', 'assignment.id' );
+    $phone_call_mod->where( 'assignment.user_id', '=', $this->id );
+    $phone_call_mod->where( 'phone_call.end_datetime', '=', NULL );
+    $phone_call_mod->order_desc( 'phone_call.start_datetime' );
+    $phone_call_list = $phone_call_class_name::select_objects( $phone_call_mod );
+    if( 1 < count( $phone_call_list ) )
+      log::warning( sprintf( 'User %d (%s) has more than one open phone call!', $this->id, $this->name ) );
+    return 0 < count( $phone_call_list ) ? current( $phone_call_list ) : NULL;
+  }
 }
