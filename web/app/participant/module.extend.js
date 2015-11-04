1a9168e8047a6ce3a9d50bb107ab706acc2713e0
Patrick Emond <emondpd@mcmaster.ca>
2015-11-04 14:48:24 -0500

    Working on participant history

--- web/app/participant/module.extend.js
+++ web/app/participant/module.extend.js
@@ -27,4 +27,147 @@ define( [ module.url + 'module.js' ], function() {
     }
   } );
 
+  angular.extend( module.historyCategoryList, {
+
+    // appointments and callbacks are added in the assignment's promise function below
+    Appointment: { active: true },
+    Callback: { active: true },
+
+    Assignment: {
+      active: true,
+      promise: function( historyList, $state, CnHttpFactory, $q ) {
+        return CnHttpFactory.instance( {
+          path: 'participant/' + $state.params.identifier + '/interview',
+          data: {
+            modifier: { order: { start_datetime: true } },
+            select: { column: [ 'id' ] }
+          }
+        } ).query().then( function( response ) {
+          var promiseArray = [];
+          response.data.forEach( function( item ) {
+            // appointments
+            promiseArray.push(
+              CnHttpFactory.instance( {
+                path: 'interview/' + item.id + '/appointment',
+                data: {
+                  modifier: { order: { start_datetime: true } },
+                  select: {
+                    column: [ 'datetime', 'type', 'reached', 'assignment_id', 'user_id', {
+                      table: 'user',
+                      column: 'first_name',
+                      alias: 'user_first'
+                    }, {
+                      table: 'user',
+                      column: 'last_name',
+                      alias: 'user_last'
+                    } ]
+                  }
+                }
+              } ).query().then( function( response ) {
+                response.data.forEach( function( item ) {
+                  var description = 'A ' + item.type + ' appointment scheduled for this time has ';
+                  description += item.assignment_id
+                               ? 'been met.\nDuring the call the participant was ' +
+                                 ( item.reached ? 'reached' : 'not reached' ) + '.\n'
+                               : 'net yet been met.';
+                  historyList.push( {
+                    datetime: item.datetime,
+                    category: 'Appointment',
+                    title: 'scheduled for ' +
+                      ( null == item.user_id ? 'any operator' : item.user_first + ' ' + item.user_last ),
+                    description: description
+                  } );
+                } );
+              } )
+            );
+
+            // assignments
+            promiseArray.push(
+              CnHttpFactory.instance( {
+                path: 'interview/' + item.id + '/assignment',
+                data: {
+                  modifier: { order: { start_datetime: true } },
+                  select: {
+                    column: [ 'start_datetime', 'end_datetime', {
+                      table: 'user',
+                      column: 'first_name',
+                      alias: 'user_first'
+                    }, {
+                      table: 'user',
+                      column: 'last_name',
+                      alias: 'user_last'
+                    }, {
+                      table: 'site',
+                      column: 'name',
+                      alias: 'site'
+                    }, {
+                      table: 'script',
+                      column: 'name',
+                      alias: 'script'
+                    }, {
+                      table: 'queue',
+                      column: 'name',
+                      alias: 'queue'
+                    } ]
+                  }
+                }
+              } ).query().then( function( response ) {
+                response.data.forEach( function( item ) {
+                  if( null != item.start_datetime ) {
+                    historyList.push( {
+                      datetime: item.start_datetime,
+                      category: 'Assignment',
+                      title: 'started by ' + item.user_first + ' ' + item.user_last,
+                      description: 'Started an assignment for the "' + item.script + '" questionnaire.\n' +
+                                   'Assigned from the ' + item.site + ' site ' +
+                                   'from the "' + item.queue + '" queue.'
+                    } );
+                  }
+                  if( null != item.end_datetime ) {
+                    historyList.push( {
+                      datetime: item.end_datetime,
+                      category: 'Assignment',
+                      title: 'completed by ' + item.user_first + ' ' + item.user_last,
+                      description: 'Completed an assignment for the "' + item.script + '" questionnaire.\n' +
+                                   'Assigned from the ' + item.site + ' site ' +
+                                   'from the "' + item.queue + '" queue.'
+                    } );
+                  }
+                } );
+              } )
+            );
+
+            // callbacks
+            promiseArray.push(
+              CnHttpFactory.instance( {
+                path: 'interview/' + item.id + '/callback',
+                data: {
+                  modifier: { order: { start_datetime: true } },
+                  select: { column: [ 'datetime', 'reached', 'assignment_id' ] }
+                }
+              } ).query().then( function( response ) {
+                response.data.forEach( function( item ) {
+                  var description = 'A callback scheduled for this time has ';
+                  description += item.assignment_id
+                               ? 'been met.\nDuring the call the participant was ' +
+                                 ( item.reached ? 'reached' : 'not reached' ) + '.\n'
+                               : 'net yet been met.';
+                  historyList.push( {
+                    datetime: item.datetime,
+                    category: 'Callback',
+                    title: 'scheduled',
+                    description: description
+                  } );
+                } );
+              } )
+            );
+
+          } );
+          return $q.all( promiseArray );
+        } );
+      }
+    },
+
+  } );
+
 } );
