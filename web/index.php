d29f59fae8256e479a080bdb5ef0fe1d34a82d98
Patrick Emond <emondpd@mcmaster.ca>
2015-11-20 17:29:15 -0500

    Fixing bugs

--- web/index.php
+++ web/index.php
@@ -6,7 +6,8 @@
 namespace sabretooth;
 use cenozo\lib, cenozo\log, sabretooth\util;
 
-if( 0 == preg_match( '#/app/[^/]+/[^/]+.extend.js#', $_SERVER['REDIRECT_URL'] ) )
+if( !array_key_exists( 'REDIRECT_URL', $_SERVER ) ||
+    0 == preg_match( '#/app/[^/]+/[^/]+.extend.js#', $_SERVER['REDIRECT_URL'] ) )
 {
   // load web-script common code
   require_once '../settings.ini.php';
