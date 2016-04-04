a18dbabc5f91383fb2ac1f58058dfeb86e64b3b8
Patrick Emond <emondpd@mcmaster.ca>
2016-04-04 15:31:29 -0400

    Bumping version

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.0.0';
-$SETTINGS['general']['build'] = 'c9d9117';
+$SETTINGS['general']['build'] = '15aaf46';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
