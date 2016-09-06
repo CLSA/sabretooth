5295f857c9e8452783be9d5ab81e52f13185f45e
Patrick Emond <emondpd@mcmaster.ca>
2016-09-06 12:57:53 -0400

    Correcting user extend module's injected files

--- web/app/user/module.extend.js
+++ web/app/user/module.extend.js
@@ -1,7 +1,7 @@
 // extend the framework's module
-define( [ 'appointment', 'shift', 'user' ].reduce( function( list, name ) {
+define( [ 'appointment', 'shift' ].reduce( function( list, name ) {
   return list.concat( cenozoApp.module( name ).getRequiredFiles() );
-}, [] ), function() {
+}, [ cenozoApp.module( 'user' ).getFileUrl( 'module.js' ) ] ), function() {
   'use strict';
 
   var module = cenozoApp.module( 'user' );
@@ -151,41 +151,6 @@ define( [ 'appointment', 'shift', 'user' ].reduce( function( list, name ) {
       };
 
       return { instance: function( parentModel ) { return new object( parentModel ); } };
-    }
-  ] );
-
-  // extend the view factory
-  cenozo.providers.decorator( 'CnUserViewFactory', [
-    '$delegate', 'CnHttpFactory',
-    function( $delegate, CnHttpFactory ) {
-      var instance = $delegate.instance;
-      $delegate.instance = function( parentModel, root ) {
-        var object = instance( parentModel, root );
-        object.afterView( function() {
-          CnHttpFactory.instance( {
-            path: 'user/' + object.record.id + '/assignment',
-            data: {
-              modifier: { where: { column: 'assignment.end_datetime', operator: '=', value: null } },
-              select: { column: [ 'id' ] }
-            }
-          } ).get().then( function( response ) {
-            if( 0 < response.data.length ) {
-              // add the view assignment button
-              module.addExtraOperation( 'view', {
-                title: 'View Active Assignment',
-                operation: function( $state, model ) {
-                  $state.go( 'assignment.view', { identifier: response.data[0].id } );
-                }
-              } );
-            } else {
-              // remove the view assignment button, if found
-              module.removeExtraOperation( 'view', 'View Active Assignment' );
-            }
-          } );
-        } );
-        return object;
-      };
-      return $delegate;
     }
   ] );
 
