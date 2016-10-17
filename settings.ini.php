f11e66d346723afdaaad569d6b9433de847d18ef
Patrick Emond <emondpd@mcmaster.ca>
2016-10-17 12:21:11 -0400

    Closing script window when ending call, advancing, reloading page, etc

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.0.0';
-$SETTINGS['general']['build'] = '278f058';
+$SETTINGS['general']['build'] = '2ccbab7';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
--- web/app/assignment/module.extend.js
+++ web/app/assignment/module.extend.js
@@ -23,20 +23,23 @@ define( [ 'participant' ].reduce( function( list, name ) {
 
   /* ######################################################################################################## */
   cenozo.providers.directive( 'cnAssignmentControl', [
-    'CnAssignmentControlFactory', '$window',
-    function( CnAssignmentControlFactory, $window ) {
+    'CnAssignmentControlFactory', 'CnSession', '$window',
+    function( CnAssignmentControlFactory, CnSession, $window ) {
       return {
         templateUrl: cenozoApp.getFileUrl( 'assignment', 'control.tpl.html' ),
         restrict: 'E',
         controller: function( $scope ) {
           $scope.model = CnAssignmentControlFactory.instance();
-          $scope.model.onLoad(); // breadcrumbs are handled by the service
+          $scope.model.onLoad( false ); // breadcrumbs are handled by the service
         },
         link: function( scope ) {
           // update the script list whenever we regain focus since there may have been script activity
           var focusFn = function() { if( null != scope.model.assignment ) scope.model.loadScriptList(); };
           var win = angular.element( $window ).on( 'focus', focusFn );
           scope.$on( '$destroy', function() { win.off( 'focus', focusFn ); } );
+
+          // close the session's script window whenever this page is unloaded (refreshed or closed)
+          $window.onunload = function() { CnSession.closeScript(); };
         }
       };
     }
@@ -50,6 +53,7 @@ define( [ 'participant' ].reduce( function( list, name ) {
               CnParticipantModelFactory, CnScriptLauncherFactory, CnModalMessageFactory, CnModalConfirmFactory ) {
       var object = function( root ) {
         var self = this;
+        this.scriptLauncher = null;
 
         this.reset = function() {
           self.isOperator = false;
@@ -161,10 +165,13 @@ define( [ 'participant' ].reduce( function( list, name ) {
         // override the onChoose function
         this.participantModel.listModel.onSelect = this.beginAssignment;
 
-        this.onLoad = function() {
+        this.onLoad = function( closeScript ) {
+          if( angular.isUndefined( closeScript ) ) closeScript = true;
           self.reset();
           self.isAssignmentLoading = true;
           self.isPrevAssignmentLoading = true;
+
+          if( closeScript ) CnSession.closeScript();
           return CnHttpFactory.instance( {
             path: 'assignment/0',
             data: { select: { column: [ 'id', 'interview_id', 'start_datetime',
@@ -357,11 +364,12 @@ define( [ 'participant' ].reduce( function( list, name ) {
         };
 
         this.launchScript = function( script ) {
-          CnScriptLauncherFactory.instance( {
+          this.scriptLauncher = CnScriptLauncherFactory.instance( {
             script: script,
             identifier: 'uid=' + self.participant.uid,
             lang: self.participant.language_code
-          } ).launch().then( function() { self.loadScriptList(); } );
+          } )
+          this.scriptLauncher.launch().then( function() { self.loadScriptList(); } );
         };
 
         this.advanceQnaire = function() {
