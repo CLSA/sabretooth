27623837abd427152ba517dca0589bfac7261c52
Patrick Emond <emondpd@mcmaster.ca>
2016-10-21 17:26:18 -0400

    Working on participant export

--- db/upgrade/2.0.0/export_column.sql
+++ db/upgrade/2.0.0/export_column.sql
@@ -5,16 +5,17 @@ CREATE TABLE IF NOT EXISTS export_column (
   update_timestamp TIMESTAMP NOT NULL,
   create_timestamp TIMESTAMP NOT NULL,
   export_id INT UNSIGNED NOT NULL,
-  rank INT UNSIGNED NOT NULL,
   table_name VARCHAR(45) NOT NULL,
+  subtype VARCHAR(45) NULL,
   column_name VARCHAR(45) NOT NULL,
-  subtype VARCHAR(45) NULL DEFAULT NULL,
+  rank INT UNSIGNED NOT NULL,
   PRIMARY KEY (id),
   INDEX fk_export_id (export_id ASC),
-  UNIQUE INDEX uq_export_id_rank (export_id ASC, rank ASC),
+  UNIQUE INDEX uq_export_rank (export_id ASC, rank ASC),
+  INDEX dk_table_name_subtype (table_name ASC, subtype ASC),
   CONSTRAINT fk_export_column_export_id
     FOREIGN KEY (export_id)
     REFERENCES export (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- db/upgrade/2.0.0/export_restriction.sql
+++ db/upgrade/2.0.0/export_restriction.sql
@@ -5,25 +5,20 @@ CREATE TABLE IF NOT EXISTS export_restriction (
   update_timestamp TIMESTAMP NOT NULL,
   create_timestamp TIMESTAMP NOT NULL,
   export_id INT UNSIGNED NOT NULL,
-  export_column_id INT UNSIGNED NOT NULL,
-  rank INT UNSIGNED NOT NULL,
+  table_name VARCHAR(45) NOT NULL,
+  subtype VARCHAR(45) NULL,
   column_name VARCHAR(45) NOT NULL,
-  logic ENUM('or', 'and') NOT NULL DEFAULT 'and',
-  test ENUM('<=>', '<>', '<', '>', 'like', 'not like') NOT NULL DEFAULT '<=>',
-  value VARCHAR(255) NULL DEFAULT NULL,
+  rank INT UNSIGNED NOT NULL,
+  logic ENUM('or', 'and') NOT NULL,
+  test ENUM('<=>', '<>', '<', '>', 'like', 'not like') NOT NULL,
+  value VARCHAR(255) NULL,
   PRIMARY KEY (id),
   INDEX fk_export_id (export_id ASC),
   UNIQUE INDEX uq_export_id_rank (export_id ASC, rank ASC),
-  INDEX fk_export_restriction_export_column_id (export_column_id ASC),
+  INDEX dk_table_name_subtype (table_name ASC, subtype ASC),
   CONSTRAINT fk_export_restriction_export_id
     FOREIGN KEY (export_id)
     REFERENCES export (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT fk_export_restriction_export_column_id
-    FOREIGN KEY (export_column_id)
-    REFERENCES export_column (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
