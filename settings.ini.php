53ffd3a9dda720766c613ffdd0ed662b06adc794
Patrick Emond <emondpd@mcmaster.ca>
2016-12-01 14:22:05 -0500

    Adding message in assignment control when participant has no phone numbers

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.0.0';
-$SETTINGS['general']['build'] = '1d48d6e';
+$SETTINGS['general']['build'] = '2214d5e';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
