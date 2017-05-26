9b00d48871c4dac30baa952b6baf0060bcac106d
Patrick Emond <emondpd@mcmaster.ca>
2017-05-26 10:29:21 -0400

    Fixing bug which caused appointments to go without an outcome after completing an assignment

--- src/database/assignment.class.php
+++ src/database/assignment.class.php
@@ -52,62 +52,12 @@ class assignment extends \cenozo\database\assignment
     // set the assignment and outcome columns in appointments
     if( $db_queue->from_appointment() )
     {
-      $modifier2 = NULL;
+      // if complete then search for associated appointments, otherwise search for unassociated ones
       $modifier = lib::create( 'database\modifier' );
-      $modifier->order( 'datetime' );
-      $modifier->limit( 1 );
+      $modifier->where( 'assignment_id', '=', $completed ? $this->id : NULL );
 
-      // if the assignment is complete then the appointment is already associated with it
-      if( $completed ) $modifier->where( 'assignment_id', '=', $this->id );
-      // if the assignment is not complete then we have to find the unassigned appointment
-      else
+      foreach( $db_interview->get_appointment_object_list( $modifier ) as $db_appointment )
       {
-        // get the pre-call window setting
-        $db_site = $db_participant->get_effective_site();
-        $pre_call_window = is_null( $db_site ) ? 0 : $db_site->get_setting()->pre_call_window;
-
-        // make sure not to select future appointments
-        $modifier->where(
-          sprintf( 'appointment.datetime - INTERVAL %d MINUTE', $pre_call_window ),
-          '<=',
-          $this->start_datetime->format( 'Y-m-d H:i:s' )
-        );
-
-        // make a copy of the modifier to use below, if needed
-        $modifier2 = clone $modifier;
-
-        // only select unassigned appointments
-        $modifier->where( 'assignment_id', '=', NULL );
-      }
-
-
-      $appointment_list = $db_interview->get_appointment_object_list( $modifier );
-      $db_appointment = NULL;
-      if( count( $appointment_list ) ) $db_appointment = current( $appointment_list );
-      else if( !is_null( $modifier2 ) )
-      {
-        // no appointment found, check to see if there is an appointment in a "broken" state
-        // Broken appointments are assigned to completed assignements but have no outcome
-        $modifier2->order_desc( 'datetime' );
-        $appointment_list = $db_interview->get_appointment_object_list( $modifier2 );
-        if( count( $appointment_list ) )
-        {
-          $db_possible_appointment = current( $appointment_list );
-          if( is_null( $db_possible_appointment->outcome ) )
-          {
-            $db_old_assignment = $db_possible_appointment->get_assignment();
-            // use this appointment if it is already assigned to this assignment, or the assignment
-            // it is assigned to has ended (since the appointment has no outcome)
-            if( $db_old_assignment->id == $this->id || !is_null( $db_old_assignment->end_datetime ) )
-              $db_appointment = $db_possible_appointment;
-          }
-        }
-      }
-
-      if( !is_null( $db_appointment ) )
-      {
-        $db_appointment = current( $appointment_list );
-
         // if the assignment is complete then set the appointment's outcome property
         if( $completed )
         {
