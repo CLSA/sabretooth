75e4609627a28a278150eb83ba376f5ce49a6ca9
Patrick Emond <emondpd@mcmaster.ca>
2017-07-04 10:34:26 -0400

    Spacing the assignment control UI better

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.1.0';
-$SETTINGS['general']['build'] = '8e57a92';
+$SETTINGS['general']['build'] = '33c7c4e';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
