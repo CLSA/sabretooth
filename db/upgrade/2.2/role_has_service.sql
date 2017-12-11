f599f74c4b32d49bcda773a627c5c741568c4bd0
Patrick Emond <emondpd@mcmaster.ca>
2017-12-11 12:09:42 -0500

    Changing services based on new hold/proxy/trace design

--- db/upgrade/2.2/role_has_service.sql
+++ db/upgrade/2.2/role_has_service.sql
@@ -10,15 +10,27 @@ CREATE PROCEDURE patch_role_has_service()
       WHERE constraint_schema = DATABASE()
       AND constraint_name = "fk_access_site_id" );
 
-    SELECT "Adding access to new hold module" AS "";
+    SELECT "Adding access to new modules" AS "";
 
-    -- add hold access to all roles
+    -- add hold/proxy/trace access to all roles
     SET @sql = CONCAT(
       "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
       "SELECT role.id, service.id ",
       "FROM ", @cenozo, ".role, service ",
       "WHERE service.restricted = 1 ",
-      "AND service.subject = 'hold'" );
+      "AND service.subject IN( 'hold', 'proxy', 'trace' )" );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
+    -- add hold_type/proxy_type/trace_type access to administrators and curators
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE service.restricted = 1 ",
+      "AND role.name IN( 'administrator', 'curator' ) ",
+      "AND service.subject IN( 'hold_type', 'proxy_type', 'trace_type' )" );
     PREPARE statement FROM @sql;
     EXECUTE statement;
     DEALLOCATE PREPARE statement;
--- db/upgrade/2.2/service.sql
+++ db/upgrade/2.2/service.sql
@@ -1,8 +1,22 @@
 SELECT "Replacing old state services with hold_type services" AS "";
 
-UPDATE service SET subject = "hold_type" WHERE subject = "state";
+DELETE FROM service WHERE subject = "state";
 
 INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'hold', 'GET', 0, 0 ),
 ( 'hold', 'GET', 1, 0 ),
-( 'hold', 'POST', 0, 1 );
+( 'hold', 'POST', 0, 1 ),
+( 'hold_type', 'DELETE', 1, 1 ),
+( 'hold_type', 'GET', 0, 0 ),
+( 'hold_type', 'GET', 1, 0 ),
+( 'hold_type', 'PATCH', 1, 1 ),
+( 'hold_type', 'POST', 0, 1 ),
+( 'proxy', 'GET', 0, 0 ),
+( 'proxy', 'GET', 1, 0 ),
+( 'proxy', 'POST', 0, 1 ),
+( 'proxy_type', 'GET', 0, 0 ),
+( 'proxy_type', 'GET', 1, 0 ),
+( 'trace', 'GET', 0, 0 ),
+( 'trace', 'GET', 1, 0 ),
+( 'trace_type', 'GET', 0, 0 ),
+( 'trace_type', 'GET', 1, 0 );
