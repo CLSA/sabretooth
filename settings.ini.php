4651e63f950f89279692703a55a3e58844deeab0
Patrick Emond <emondpd@mcmaster.ca>
2018-01-02 17:27:39 -0500

    Removing development_mode setting from settings.ini.php
    This is being done because it was interfering with the master development_mode setting in Cenozo's ini file

--- settings.ini.php
+++ settings.ini.php
@@ -15,9 +15,6 @@ $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name']
 $SETTINGS['general']['version'] = '2.2';
 $SETTINGS['general']['build'] = '47ae8c1';
 
-// always leave as false when running as production server
-$SETTINGS['general']['development_mode'] = false;
-
 // the location of sabretooth internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
 
