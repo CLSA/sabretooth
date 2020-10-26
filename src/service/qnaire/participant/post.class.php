9e924f9b4ef468995c7b32439c5476f412d97a8c
Patrick Emond <emondpd@mcmaster.ca>
2020-10-26 15:29:33 -0400

    fixing incorrect coding format

--- src/service/qnaire/participant/post.class.php
+++ src/service/qnaire/participant/post.class.php
@@ -33,9 +33,7 @@ class post extends \cenozo\service\write
       $file = $this->get_file_as_object();
       if( !property_exists( $file, 'mode' ) ||
           !in_array( $file->mode, ['confirm', 'update'] ) ||
-          !property_exists( $file, 'identifier_list' ) ) {
-        $this->status->set_code( 400 );
-      }
+          !property_exists( $file, 'identifier_list' ) ) $this->status->set_code( 400 );
     }
   }
 
