2b0b32716a6870b1e22106bbf25ba12b987d9b25
Patrick Emond <emondpd@mcmaster.ca>
2020-12-15 16:14:34 -0500

    Giving supervisors access to study and stratum

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.6';
-$SETTINGS['general']['build'] = '920da1a';
+$SETTINGS['general']['build'] = '2c5e8fb';
 
 // determines the vacancy block size
 $SETTINGS['general']['vacancy_size'] = 30;
