31d93ac66a8d6e023821488514ce103ecf93f043
Patrick Emond <emondpd@mcmaster.ca>
2021-06-11 10:47:18 -0400

    Fixing async bugs

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.7';
-$SETTINGS['general']['build'] = '30d13d45';
+$SETTINGS['general']['build'] = 'd07627c2';
 
 // determines the vacancy block size
 $SETTINGS['general']['vacancy_size'] = 30;
