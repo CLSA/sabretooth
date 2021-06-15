996186bbe420688802d120571cb89871a3c7cc1b
Patrick Emond <emondpd@mcmaster.ca>
2021-06-15 10:30:58 -0400

    Fixing logout bug

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.7';
-$SETTINGS['general']['build'] = '1068b78b';
+$SETTINGS['general']['build'] = '89091a05';
 
 // determines the vacancy block size
 $SETTINGS['general']['vacancy_size'] = 30;
