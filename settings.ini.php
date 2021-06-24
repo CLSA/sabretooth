571c8ae50b41f7ed1d8cc4f0a07b9a527f4307e6
Patrick Emond <emondpd@mcmaster.ca>
2021-06-24 14:28:39 -0400

    Fixing typo

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.7';
-$SETTINGS['general']['build'] = '89091a05';
+$SETTINGS['general']['build'] = '996186bb';
 
 // determines the vacancy block size
 $SETTINGS['general']['vacancy_size'] = 30;
