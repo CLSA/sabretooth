e1b242408ab0285f8db2d28af15286e9911b94cc
Patrick Emond <emondpd@mcmaster.ca>
2022-02-01 12:14:04 -0500

    Removing DELETE alternate_type service

--- db/upgrade/2.8/service.sql
+++ db/upgrade/2.8/service.sql
@@ -1,10 +1,11 @@
 SELECT "Adding new services" AS "";
 
 INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
-( 'alternate_type', 'DELETE', 1, 1 ),
 ( 'alternate_type', 'GET', 0, 0 ),
 ( 'alternate_type', 'GET', 1, 0 ),
 ( 'alternate_type', 'PATCH', 1, 1 ),
 ( 'alternate_type', 'POST', 0, 1 ),
 ( 'country', 'GET', 0, 0 ),
 ( 'country', 'GET', 1, 0 );
+
+DELETE FROM service WHERE subject = 'alternate_type' AND method = 'DELETE';
