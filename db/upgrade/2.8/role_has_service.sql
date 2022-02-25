649e8a3e7c1d42bd6be4143189d490f1eb4949d8
Patrick Emond <emondpd@mcmaster.ca>
2022-02-25 14:15:55 -0500

    Changing services as part of Pine/Withdraw update

--- db/upgrade/2.8/role_has_service.sql
+++ db/upgrade/2.8/role_has_service.sql
@@ -25,6 +25,18 @@ CREATE PROCEDURE patch_role_has_service()
     EXECUTE statement;
     DEALLOCATE PREPARE statement;
 
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE role.name IN( 'administrator', 'curator', 'helpline', 'operator', 'operator+', 'supervisor' ) ",
+      "AND service.subject = 'pine_response' ",
+      "AND service.restricted = 1"
+    );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
   END //
 DELIMITER ;
 
--- db/upgrade/2.8/service.sql
+++ db/upgrade/2.8/service.sql
@@ -1,4 +1,4 @@
-SELECT "Adding new services" AS "";
+SELECT 'Adding new services' AS '';
 
 INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'alternate_type', 'GET', 0, 0 ),
@@ -6,6 +6,9 @@ INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'alternate_type', 'PATCH', 1, 1 ),
 ( 'alternate_type', 'POST', 0, 1 ),
 ( 'country', 'GET', 0, 0 ),
-( 'country', 'GET', 1, 0 );
+( 'country', 'GET', 1, 0 ),
+( 'pine_response', 'POST', 0, 1 );
+
+UPDATE service SET restricted = 1 WHERE subject = 'pine_response' AND method = 'GET';
 
 DELETE FROM service WHERE subject = 'alternate_type' AND method = 'DELETE';
