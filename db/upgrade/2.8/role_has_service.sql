f22903e1cfd54563634252ebc9bc98b24c8c875f
Patrick Emond <emondpd@mcmaster.ca>
2022-07-15 14:45:12 -0400

    Adding access to new equipment module

--- db/upgrade/2.8/role_has_service.sql
+++ db/upgrade/2.8/role_has_service.sql
@@ -18,7 +18,32 @@ CREATE PROCEDURE patch_role_has_service()
       "SELECT role.id, service.id ",
       "FROM ", @cenozo, ".role, service ",
       "WHERE role.name = 'administrator' ",
-      "AND service.subject = 'alternate_type' ",
+      "AND service.subject IN( 'alternate_type', 'equipment', 'equipment_loan', 'equipment_type' ) ",
+      "AND service.restricted = 1"
+    );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE role.name IN( 'curator', 'helpline' ) ",
+      "AND service.subject IN( 'equipment', 'equipment_loan', 'equipment_type' ) ",
+      "AND service.method = 'GET' "
+      "AND service.restricted = 1"
+    );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE role.name IN( 'curator', 'supervisor' ) ",
+      "AND service.subject IN( 'equipment', 'equipment_loan' ) ",
       "AND service.restricted = 1"
     );
     PREPARE statement FROM @sql;
--- db/upgrade/2.8/service.sql
+++ db/upgrade/2.8/service.sql
@@ -7,6 +7,21 @@ INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
 ( 'alternate_type', 'POST', 0, 1 ),
 ( 'country', 'GET', 0, 0 ),
 ( 'country', 'GET', 1, 0 ),
+( 'equipment', 'DELETE', 1, 1 ),
+( 'equipment', 'GET', 0, 1 ),
+( 'equipment', 'GET', 1, 1 ),
+( 'equipment', 'PATCH', 1, 1 ),
+( 'equipment', 'POST', 0, 1 ),
+( 'equipment_loan', 'DELETE', 1, 1 ),
+( 'equipment_loan', 'GET', 0, 1 ),
+( 'equipment_loan', 'GET', 1, 1 ),
+( 'equipment_loan', 'PATCH', 1, 1 ),
+( 'equipment_loan', 'POST', 0, 1 ),
+( 'equipment_type', 'DELETE', 1, 1 ),
+( 'equipment_type', 'GET', 0, 1 ),
+( 'equipment_type', 'GET', 1, 1 ),
+( 'equipment_type', 'PATCH', 1, 1 ),
+( 'equipment_type', 'POST', 0, 1 ),
 ( 'pine_response', 'POST', 0, 1 );
 
 UPDATE service SET restricted = 1 WHERE subject = 'pine_response' AND method = 'GET';
