2b1731d96d4d56e02d51d14c95e5fa2025ffb0e2
Patrick Emond <emondpd@mcmaster.ca>
2024-06-07 12:53:13 -0400

    Feature: adding email to sample report

--- src/business/report/sample.class.php
+++ src/business/report/sample.class.php
@@ -93,7 +93,8 @@ class sample extends \cenozo\business\report\base_report
     );  
     $select->add_column( $this->get_datetime_column( 'application_has_participant.datetime' ), 'Released', false );
     $select->add_column( 'region.abbreviation', 'Province/State', false );
-    $select->add_column( 'IF( participant.email IS NOT NULL, "Yes", "No" )', 'Has Email', false );
+    $select->add_column( 'participant.email', 'Email', false );
+    $select->add_column( 'participant.email2', 'Alt Email', false );
     $select->add_column(
       $this->get_datetime_column( 'participant.callback' ),
       'Callback',
