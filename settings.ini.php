1735112befac1ec0fc5f11558ed9b8ee890f5f3b
Patrick Emond <emondpd@mcmaster.ca>
2026-04-01 10:32:52 -0400

    v3: build version needed to be updated

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'sabretooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.10';
-$SETTINGS['general']['build'] = '630d2150';
+$SETTINGS['general']['build'] = 'a363651e';
 
 // determines whether this instance is for proxy interviewing
 $SETTINGS['general']['proxy'] = false;
