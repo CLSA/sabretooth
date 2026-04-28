e092f70de67326dd36826683060d462633698279
Patrick Emond <emondpd@mcmaster.ca>
2026-04-28 11:17:03 -0400

    Bug: administrator's notation list was missing

--- db/upgrade/2.10/patch_database.sql
+++ db/upgrade/2.10/patch_database.sql
@@ -3,7 +3,9 @@
 SET AUTOCOMMIT=0;
 
 SOURCE setting.sql
+
 SOURCE service.sql
+SOURCE role_has_service.sql
 
 SOURCE update_version_number.sql
 
--- db/upgrade/2.10/service.sql
+++ db/upgrade/2.10/service.sql
@@ -1,5 +1,7 @@
 SELECT 'Adding new services' AS '';
 
 INSERT IGNORE INTO service ( subject, method, resource, restricted ) VALUES
+( 'notation', 'GET', 0, 1 ),
+( 'notation', 'GET', 1, 1 ),
 ( 'user_ip_address', 'GET', 0, 0 ),
 ( 'user_ip_address', 'GET', 1, 0 );
